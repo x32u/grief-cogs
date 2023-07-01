8f926750838ea4220e6c3cd1e741582cea0176f6
resent <contact@resent.dev>
2023-07-01 06:39:18 -0400

    Transferring cogs from VPS to repo for ease

---
+++ pp/__init__.py
@@ -0,0 +1,7 @@
+from pp.pp import Pp
+
+__red_end_user_data_statement__ = "This cog does not store any end user data."
+
+
+async def setup(bot):
+    await bot.add_cog(Pp(bot))
---
+++ pp/pp.py
@@ -0,0 +1,70 @@
+import random
+
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+from redbot.core.utils.chat_formatting import pagify
+
+
+class Pp(commands.Cog):
+    """
+    Shows your or someone else's pp length.
+
+    Note - 100% accurate
+
+    """
+
+    def __init__(self, bot: Red):
+        self.bot = bot
+        default = {"random": True}
+        self.config = Config.get_conf(self, 694835810347909161, force_registration=True)
+        self.config.register_global(**default)
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """
+        Nothing to delete.
+        """
+        return
+
+    @commands.command(name="randompp")
+    @commands.is_owner()
+    async def randompp(self, ctx, toggle: bool):
+        """
+        Set whether or not to generate a new random pp length every time the
+        command is ran.
+        """
+        await self.config.random.set(toggle)
+        await ctx.send(f'Random pp length is now {"enabled" if toggle else "disabled"}.')
+
+    @commands.command(name="pp")
+    async def pp(self, ctx, *users: discord.Member):
+        """
+        Detects a user's pp length.
+
+        Note : This is 100% accurate.
+
+        Enter multiple users for a comparison!
+
+        """
+        if not users:
+            users = {ctx.author}
+
+        lengths = {}
+
+        for user in users:
+            random.seed(None) if await self.config.random() else random.seed(str(user.id))
+
+            if user.id == ctx.bot.user.id or user.id in list(ctx.bot.owner_ids):
+                length = random.randint(30, 35)
+            else:
+                length = random.randint(0, 30)
+
+            lengths[user] = f'8{"=" * length}D'
+
+        lengths = sorted(lengths.items(), key=lambda x: x[1])
+
+        msg = "".join(f"**{user.display_name}'s size:**\n{length}\n" for user, length in lengths)
+
+        for page in pagify(msg):
+            await ctx.send(page)
