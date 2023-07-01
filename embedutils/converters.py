083f62dda2d0ce8abd5a4737722b003f6e0cbf6f
resent <contact@resent.dev>
2023-07-01 06:33:26 -0400

    Transferring cogs from VPS to repo for ease

---
+++ embedutils/converters.py
@@ -0,0 +1,257 @@
+"""
+MIT License
+
+Copyright (c) 2020-present phenom4n4n
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import asyncio
+import json
+import re
+from typing import Dict, List, Optional, Union
+
+import discord
+import yaml
+from redbot.core import commands
+from redbot.core.utils import menus
+
+PASTEBIN_RE = re.compile(r"(?:https?://(?:www\.)?)?pastebin\.com/(?:raw/)?([a-zA-Z0-9]+)")
+
+
+class StringToEmbed(commands.Converter[discord.Embed]):
+    def __init__(
+        self, *, conversion_type: str = "json", validate: bool = True, content: bool = False
+    ):
+        self.CONVERSION_TYPES = {
+            "json": self.load_from_json,
+            "yaml": self.load_from_yaml,
+        }
+
+        self.validate = validate
+        self.conversion_type = conversion_type.lower()
+        self.allow_content = content
+        try:
+            self.converter = self.CONVERSION_TYPES[self.conversion_type]
+        except KeyError as exc:
+            raise ValueError(
+                f"{conversion_type} is not a valid conversion type for Embed conversion."
+            ) from exc
+
+    async def convert(self, ctx: commands.Context, argument: str) -> discord.Embed:
+        data = argument.strip("`")
+        data = await self.converter(ctx, data)
+        content = self.get_content(data)
+
+        if data.get("embed"):
+            data = data["embed"]
+        elif data.get("embeds"):
+            data = data.get("embeds")[0]
+        self.check_data_type(ctx, data)
+
+        fields = await self.create_embed(ctx, data, content=content)
+        content = fields["content"]
+        embed = fields["embed"]
+        if self.validate:
+            await self.validate_embed(ctx, embed, content=content)
+        return embed
+
+    def check_data_type(self, ctx: commands.Context, data, *, data_type=dict):
+        if not isinstance(data, data_type):
+            raise commands.BadArgument(
+                f"This doesn't seem to be properly formatted embed {self.conversion_type.upper()}. "
+                f"Refer to the link on `{ctx.clean_prefix}help {ctx.command.qualified_name}`."
+            )
+
+    async def load_from_json(self, ctx: commands.Context, data: str, **kwargs) -> dict:
+        try:
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError as error:
+            await self.embed_convert_error(ctx, "JSON Parse Error", error)
+        self.check_data_type(ctx, data, **kwargs)
+        return data
+
+    async def load_from_yaml(self, ctx: commands.Context, data: str, **kwargs) -> dict:
+        try:
+            data = yaml.safe_load(data)
+        except Exception as error:
+            await self.embed_convert_error(ctx, "YAML Parse Error", error)
+        self.check_data_type(ctx, data, **kwargs)
+        return data
+
+    def get_content(self, data: dict, *, content: str = None) -> Optional[str]:
+        content = data.pop("content", content)
+        if content is not None and not self.allow_content:
+            raise commands.BadArgument("The `content` field is not supported for this command.")
+        return content
+
+    async def create_embed(
+        self, ctx: commands.Context, data: dict, *, content: str = None
+    ) -> Dict[str, Union[discord.Embed, str]]:
+        content = self.get_content(data, content=content)
+
+        if timestamp := data.get("timestamp"):
+            data["timestamp"] = timestamp.strip("Z")
+        try:
+            e = discord.Embed.from_dict(data)
+            length = len(e)
+        except Exception as error:
+            await self.embed_convert_error(ctx, "Embed Parse Error", error)
+
+        # Embed.__len__ may error which is why it is included in the try/except
+        if length > 6000:
+            raise commands.BadArgument(
+                f"Embed size exceeds Discord limit of 6000 characters ({length})."
+            )
+        return {"embed": e, "content": content}
+
+    async def validate_embed(
+        self, ctx: commands.Context, embed: discord.Embed, *, content: str = None
+    ):
+        try:
+            await ctx.channel.send(content, embed=embed)  # ignore tips/monkeypatch cogs
+        except discord.errors.HTTPException as error:
+            await self.embed_convert_error(ctx, "Embed Send Error", error)
+
+    @staticmethod
+    async def embed_convert_error(ctx: commands.Context, error_type: str, error: Exception):
+        embed = discord.Embed(
+            color=await ctx.embed_color(),
+            title=f"{error_type}: `{type(error).__name__}`",
+            description=f"```py\n{error}\n```",
+        )
+        embed.set_footer(
+            text=f"Use `{ctx.prefix}help {ctx.command.qualified_name}` to see an example"
+        )
+        asyncio.create_task(menus.menu(ctx, [embed], {"❌": menus.close_menu}))
+        raise commands.CheckFailure()
+
+
+class ListStringToEmbed(StringToEmbed):
+    def __init__(self, *, conversion_type: str = "json", limit: int = 10):
+        super().__init__(conversion_type=conversion_type)
+        self.limit = limit
+
+    async def convert(self, ctx: commands.Context, argument: str) -> List[discord.Embed]:
+        data = argument.strip("`")
+        data = await self.converter(ctx, data, data_type=(dict, list))
+
+        if isinstance(data, list):
+            pass
+        elif data.get("embed"):
+            data = [data["embed"]]
+        elif data.get("embeds"):
+            data = data.get("embeds")
+            if isinstance(data, dict):
+                data = list(data.values())
+        else:
+            data = [data]
+        self.check_data_type(ctx, data, data_type=list)
+
+        embeds = []
+        for i, embed_data in enumerate(data, 1):
+            fields = await self.create_embed(ctx, embed_data)
+            embed = fields["embed"]
+            embeds.append(embed)
+            if i >= self.limit:
+                raise commands.BadArgument(f"Embed limit reached ({self.limit}).")
+        if embeds:
+            return embeds
+        else:
+            raise commands.BadArgument("Failed to convert input into embeds.")
+
+
+class EmbedNotFound(commands.BadArgument):
+    def __init__(self, name: str, *, is_global: bool = False):
+        embed = "Global embed" if is_global else "Embed"
+        super().__init__(f'{embed} "{name}" not found.')
+
+
+class StoredEmbedConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, name: str) -> dict:
+        cog = ctx.bot.get_cog("EmbedUtils")
+        data = await cog.config.guild(ctx.guild).embeds()
+        embed = data.get(name)
+        if not embed:
+            raise EmbedNotFound(name)
+        embed.update(name=name)
+        return embed
+
+
+class GlobalStoredEmbedConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, name: str) -> dict:
+        cog = ctx.bot.get_cog("EmbedUtils")
+        data = await cog.config.embeds()
+        embed = data.get(name)
+        if not embed:
+            raise EmbedNotFound(name, is_global=True)
+        can_view = await ctx.bot.is_owner(ctx.author) or not embed.get("locked")
+        if embed and can_view:
+            embed.update(name=name)
+            return embed
+        raise EmbedNotFound(name, is_global=True)
+
+
+class MyMessageConverter(commands.MessageConverter):
+    async def convert(self, ctx: commands.Context, argument: str) -> discord.Message:
+        message = await super().convert(ctx, argument)
+        if message.author.id != ctx.me.id:
+            raise commands.BadArgument("That is not a message sent by me.")
+        elif not message.channel.permissions_for(ctx.me).send_messages:
+            raise commands.BadArgument(
+                f"I do not have permissions to send/edit messages in {message.channel.mention}."
+            )
+        return message
+
+
+class MessageableChannel(commands.TextChannelConverter):
+    async def convert(self, ctx: commands.Context, argument: str) -> discord.TextChannel:
+        channel = await super().convert(ctx, argument)
+        my_perms = channel.permissions_for(ctx.me)
+        if not (my_perms.send_messages and my_perms.embed_links):
+            raise commands.BadArgument(
+                f"I do not have permissions to send embeds in {channel.mention}."
+            )
+        author_perms = channel.permissions_for(ctx.author)
+        if not (author_perms.send_messages and author_perms.embed_links):
+            raise commands.BadArgument(
+                f"You do not have permissions to send embeds in {channel.mention}."
+            )
+        return channel
+
+
+class PastebinMixin:
+    async def convert(self, ctx: commands.Context, argument: str) -> str:
+        match = PASTEBIN_RE.match(argument)
+        if not match:
+            raise commands.BadArgument(f"`{argument}` is not a valid Pastebin link.")
+        paste_id = match.group(1)
+        async with ctx.cog.session.get(f"https://pastebin.com/raw/{paste_id}") as resp:
+            if resp.status != 200:
+                raise commands.BadArgument(f"`{argument}` is not a valid Pastebin link.")
+            embed_data = await resp.text()
+        return await super().convert(ctx, embed_data)
+
+
+class PastebinConverter(PastebinMixin, StringToEmbed):
+    ...
+
+
+class PastebinListConverter(PastebinMixin, ListStringToEmbed):
+    ...
---
+++ errorlogs/reaction_menu.py
@@ -0,0 +1,220 @@
+import asyncio
+import contextlib
+import inspect
+import itertools
+import pathlib
+from collections import deque
+from typing import Awaitable, Callable, Dict, List
+
+import discord
+from redbot.core import commands
+from redbot.core.utils import chat_formatting as chatutils, menus as menutils
+
+MAX_CONTENT_SIZE = 2000 - len("```ini```\n\n")
+
+
+def button(emoji: str):
+    def decorator(func):
+        try:
+            func.__react_to__.append(emoji)
+        except AttributeError:
+            func.__react_to__ = [emoji]
+        return func
+
+    return decorator
+
+
+# noinspection PyUnusedLocal
+class LogScrollingMenu:
+
+    _handlers: Dict[
+        str,
+        Callable[["LogScrollingMenu", discord.RawReactionActionEvent], Awaitable[None]],
+    ] = {}
+
+    def __init__(self, ctx: commands.Context, lines: List[str], page_size: int) -> None:
+        self.ctx = ctx
+        self.message = None
+
+        self._lines = lines
+        self._page_size = page_size
+        self._end_pos = len(self._lines)
+        self._start_pos = self._end_pos - page_size
+        self._done_event = asyncio.Event()
+
+    @classmethod
+    async def send(
+        cls,
+        ctx: commands.Context,
+        logfiles: List[pathlib.Path],
+        page_size: int = 25,
+        num_pages: int = 15,
+    ):
+        lines = deque(maxlen=num_pages * page_size + 2)
+        for logfile_path in logfiles:
+            new_lines = deque(maxlen=lines.maxlen - len(lines))
+            with logfile_path.open() as fs:
+                new_lines.extend(fs.readlines())
+            lines = deque(
+                iterable=itertools.chain(new_lines, lines), maxlen=lines.maxlen
+            )
+            del new_lines
+            if len(lines) >= lines.maxlen:
+                break
+        lines.popleft()
+        lines.popleft()
+        lines.appendleft("# START OF LOG BUFFER\n")
+        lines.append("# END OF LOG\n")
+
+        self = cls(ctx, list(lines), page_size)
+
+        self.ctx.bot.add_listener(self.on_raw_reaction, "on_raw_reaction_add")
+        self.ctx.bot.add_listener(self.on_raw_reaction, "on_raw_reaction_remove")
+        try:
+            await asyncio.shield(self.wait())
+        except asyncio.CancelledError:
+            if not self._done_event.is_set() and self.message is not None:
+                with contextlib.suppress(discord.NotFound):
+                    await self.message.delete()
+        finally:
+            self.ctx.bot.remove_listener(self.on_raw_reaction, "on_raw_reaction_add")
+            self.ctx.bot.remove_listener(self.on_raw_reaction, "on_raw_reaction_remove")
+
+    async def wait(self) -> None:
+        await self._update_message()
+        await self._done_event.wait()
+
+    async def on_raw_reaction(self, payload: discord.RawReactionActionEvent) -> None:
+        if not self._same_context(payload):
+            return
+
+        try:
+            handler = self._handlers[payload.emoji.name]
+        except KeyError:
+            return
+        else:
+            await handler(self, payload)
+
+    @button("\N{UPWARDS BLACK ARROW}")
+    async def scroll_up(self, payload: discord.RawReactionActionEvent) -> None:
+        if self._start_pos <= 0:
+            return
+        self._start_pos -= 1
+        self._end_pos = self._start_pos + self._page_size
+        await self._update_message(pin="start")
+
+    @button("\N{DOWNWARDS BLACK ARROW}")
+    async def scroll_down(self, payload: discord.RawReactionActionEvent) -> None:
+        if self._end_pos >= len(self._lines):
+            return
+        self._end_pos += 1
+        self._start_pos = self._end_pos - self._page_size
+        await self._update_message(pin="end")
+
+    @button("\N{BLACK UP-POINTING DOUBLE TRIANGLE}")
+    async def page_up(self, payload: discord.RawReactionActionEvent) -> None:
+        if self._start_pos <= 0:
+            return
+        self._end_pos = self._start_pos
+        self._start_pos = max(self._end_pos - self._page_size, 0)
+        await self._update_message(pin="end")
+
+    @button("\N{BLACK DOWN-POINTING DOUBLE TRIANGLE}")
+    async def page_down(self, payload: discord.RawReactionActionEvent) -> None:
+        if self._end_pos >= len(self._lines):
+            return
+        self._start_pos = self._end_pos
+        self._end_pos = self._start_pos + self._page_size
+        await self._update_message(pin="start")
+
+    @button("\N{UP DOWN ARROW}")
+    async def expand(self, payload: discord.RawReactionActionEvent) -> None:
+        self._page_size += 2
+        if self._start_pos <= 0 and self._end_pos >= len(self._lines):
+            return
+        self._start_pos = max(self._start_pos - 1, 0)
+        self._end_pos = min(self._end_pos + 1, len(self._lines))
+        await self._update_message()
+
+    @button("\N{END WITH LEFTWARDS ARROW ABOVE}")
+    async def go_to_end(self, payload: discord.RawReactionActionEvent) -> None:
+        if self._end_pos >= len(self._lines):
+            return
+        self._end_pos = len(self._lines)
+        self._start_pos = self._end_pos - self._page_size
+        await self._update_message(pin="end")
+
+    @button("\N{CROSS MARK}")
+    async def exit_menu(self, payload: discord.RawReactionActionEvent) -> None:
+        self._done_event.set()
+        await self.message.delete()
+
+    def _same_context(self, payload: discord.RawReactionActionEvent) -> bool:
+        return (
+            payload.message_id == self.message.id
+            and payload.user_id == self.ctx.author.id
+        )
+
+    async def _update_message(self, *, pin: str = "end") -> None:
+        joined_lines = "".join(self._lines[self._start_pos : self._end_pos])
+
+        if len(joined_lines) > MAX_CONTENT_SIZE:
+            if pin == "start":
+                cutoff = joined_lines.rfind("\n", 0, MAX_CONTENT_SIZE)
+                joined_lines = joined_lines[:cutoff]
+            else:
+                cutoff = joined_lines.find("\n", -MAX_CONTENT_SIZE)
+                joined_lines = joined_lines[cutoff + 1 :]
+
+        rendered_page_size = joined_lines.count("\n")
+        if pin == "start":
+            self._end_pos = self._start_pos + rendered_page_size
+            if self._end_pos >= len(self._lines) and pin == "start":
+                while rendered_page_size < self._page_size:
+                    try:
+                        new_line = self._lines[self._start_pos - 1]
+                    except IndexError:
+                        break
+                    else:
+                        if len(joined_lines) + len(new_line) > MAX_CONTENT_SIZE:
+                            break
+                        joined_lines = new_line + joined_lines
+                        self._start_pos -= 1
+                        rendered_page_size += 1
+        elif pin == "end":
+            self._start_pos = self._end_pos - rendered_page_size
+            if self._start_pos <= 0 and pin == "end":
+                while rendered_page_size < self._page_size:
+                    try:
+                        new_line = self._lines[self._end_pos]
+                    except IndexError:
+                        break
+                    else:
+                        if len(joined_lines) + len(new_line) > MAX_CONTENT_SIZE:
+                            break
+                        joined_lines += new_line
+                        self._end_pos += 1
+                        rendered_page_size += 1
+
+        content = chatutils.box(joined_lines, lang="ini")
+        if self.message is None:
+            self.message = await self.ctx.send(content)
+            menutils.start_adding_reactions(self.message, self._handlers.keys())
+        else:
+            try:
+                await self.message.edit(content=content)
+            except discord.NotFound:
+                self._done_event.set()
+
+
+for _, _method in reversed(
+    inspect.getmembers(LogScrollingMenu, inspect.iscoroutinefunction)
+):
+    try:
+        _emojis = _method.__react_to__
+    except AttributeError:
+        continue
+    else:
+        for _emoji in _emojis:
+            # noinspection PyProtectedMember
+            LogScrollingMenu._handlers[_emoji] = _method
