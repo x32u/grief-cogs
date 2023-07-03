53629a3ff243dc72c550ff2b89f749a42ef11027
resent <contact@resent.dev>
2023-07-03 05:20:32 -0400

    Fuck author and version info

--- roleutils/roleutils.py
+++ roleutils/roleutils.py
@@ -1,28 +1,5 @@
-"""
-MIT License
 
-Copyright (c) 2020-present phenom4n4n
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-# Multi-file class combining taken from https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/redbot/cogs/mod/mod.py
 import asyncio
 import logging
 from abc import ABC
@@ -53,19 +30,6 @@ class RoleUtils(
     commands.Cog,
     metaclass=CompositeMetaClass,
 ):
-    """
-    Useful role commands.
-
-    Includes massroling, role targeting, and reaction roles.
-    """
-
-    __version__ = "1.4.0"
-
-    def format_help_for_context(self, ctx):
-        pre_processed = super().format_help_for_context(ctx)
-        n = "\n" if "\n\n" not in pre_processed else ""
-        return f"{pre_processed}{n}\nCog Version: {self.__version__}"
-
     def __init__(self, bot: Red, *_args) -> None:
         self.cache = {}
         self.bot = bot
