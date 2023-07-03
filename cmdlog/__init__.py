58c7a484f145183397d489fdb7ae987e6c8b5a3b
resent <contact@resent.dev>
2023-07-03 02:20:59 -0400

    cmdlog fix

--- cmdlog/__init__.py
+++ cmdlog/__init__.py
@@ -16,5 +16,4 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 async def setup(bot: Red):
     cog = CmdLog(bot)
-    await out_of_date_check("cmdlog", cog.__version__)
     await bot.add_cog(cog)
