221fa21db47b84ed83eeece671c57c333b1107f1
resent <contact@resent.dev>
2023-07-06 17:25:33 -0400

    Broken

--- reactionlog/__init__.py
+++
@@ -1,7 +0,0 @@
-from .reactionlog import ReactionLog
-
-__red_end_user_data_statement__ = "This cog does not store any end user data."
-
-
-async def setup(bot):
-    await bot.add_cog(ReactionLog(bot))
