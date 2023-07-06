5f0c3872f2d10d4ce30b2ac86ce4c74e32e80582
resent <contact@resent.dev>
2023-07-05 23:35:33 -0400

    Delete snipe directory

--- snipe/__init__.py
+++
@@ -1,9 +0,0 @@
-from .snipe import Snipe
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    n = Snipe(bot)
-    await n.init()
-    await bot.add_cog(n)
