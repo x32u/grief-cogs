62a0dd85ae935400c72fa967f1e0e92426baf5d5
resent <contact@resent.dev>
2023-07-20 18:36:20 -0400

    Update __init__.py

--- consolelogs/__init__.py
+++ consolelogs/__init__.py
@@ -1,7 +1,5 @@
 from redbot.core import errors  # isort:skip
 import importlib
-import json
-import os
 import sys
 
 try:
@@ -14,24 +12,26 @@ modules = sorted(
     [module for module in sys.modules if module.split(".")[0] == "AAA3A_utils"], reverse=True
 )
 for module in modules:
-    importlib.reload(sys.modules[module])
+    try:
+        importlib.reload(sys.modules[module])
+    except ModuleNotFoundError:
+        pass
 del AAA3A_utils
-import AAA3A_utils
-AAA3A_utils.dev.Cog = AAA3A_utils.Cog
-AAA3A_utils.cog.DevEnv = AAA3A_utils.DevEnv
-AAA3A_utils.dev.SharedCog = AAA3A_utils.SharedCog
-__version__ = AAA3A_utils.__version__
-with open(os.path.join(os.path.dirname(__file__), "utils_version.json"), mode="r") as f:
-    data = json.load(f)
-needed_utils_version = data["needed_utils_version"]
-if __version__ > needed_utils_version:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo."
-    )
-elif __version__ < needed_utils_version:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog has a lower version than the one supported by this version of the cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
-    )
+# import AAA3A_utils
+# import json
+# import os
+# __version__ = AAA3A_utils.__version__
+# with open(os.path.join(os.path.dirname(__file__), "utils_version.json"), mode="r") as f:
+#     data = json.load(f)
+# needed_utils_version = data["needed_utils_version"]
+# if __version__ > needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo."
+#     )
+# elif __version__ < needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a lower version than the one supported by this version of the cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
+#     )
 
 from redbot.core.bot import Red  # isort:skip
 
