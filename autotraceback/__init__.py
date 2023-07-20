90d523477e870cfd095c73516f214bd89b6ff909
resent <contact@resent.dev>
2023-07-20 18:34:54 -0400

    Update __init__.py

--- autotraceback/__init__.py
+++ autotraceback/__init__.py
@@ -1,34 +1,37 @@
 from redbot.core import errors  # isort:skip
 import importlib
-import json
-import os
 import sys
 
+try:
+    import AAA3A_utils
+except ModuleNotFoundError:
+    raise errors.CogLoadError(
+        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
+    )
 modules = sorted(
     [module for module in sys.modules if module.split(".")[0] == "AAA3A_utils"], reverse=True
 )
 for module in modules:
-    importlib.reload(sys.modules[module])
-try:
-    import AAA3A_utils
-
-    AAA3A_utils.dev.CogsUtils = AAA3A_utils.CogsUtils
-    __version__ = AAA3A_utils.__version__
-except ImportError:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
-    )
-with open(os.path.join(os.path.dirname(__file__), "utils_version.json"), mode="r") as f:
-    data = json.load(f)
-needed_utils_version = data["needed_utils_version"]
-if __version__ > needed_utils_version:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo."
-    )
-elif __version__ < needed_utils_version:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog has a lower version than the one supported by this version of the cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
-    )
+    try:
+        importlib.reload(sys.modules[module])
+    except ModuleNotFoundError:
+        pass
+del AAA3A_utils
+# import AAA3A_utils
+# import json
+# import os
+# __version__ = AAA3A_utils.__version__
+# with open(os.path.join(os.path.dirname(__file__), "utils_version.json"), mode="r") as f:
+#     data = json.load(f)
+# needed_utils_version = data["needed_utils_version"]
+# if __version__ > needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo."
+#     )
+# elif __version__ < needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a lower version than the one supported by this version of the cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
+#     )
 
 from redbot.core.bot import Red  # isort:skip
 import asyncio
@@ -48,7 +51,7 @@ async def setup_after_ready(bot: Red) -> None:
     cog = AutoTraceback(bot)
     if old_traceback := bot.get_command("traceback"):
         bot.remove_command(old_traceback.name)
-    await cog.cogsutils.add_cog(bot)
+    await bot.add_cog(cog)
 
 
 async def setup(bot: Red) -> None:
