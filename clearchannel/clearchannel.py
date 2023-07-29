05ec376295207de5b2f92cefd8cff14823aedbab
resent <contact@resent.dev>
2023-07-28 22:20:45 -0400

    test

--- clearchannel/clearchannel.py
+++ clearchannel/clearchannel.py
@@ -30,7 +30,6 @@ class ClearChannel(Cog, DashboardIntegration):
 
     async def cog_load(self):
         await super().cog_load()
-        await self.settings.add_commands()
 
     async def red_delete_data_for_user(self, *args, **kwargs) -> None:
         """Nothing to delete."""
