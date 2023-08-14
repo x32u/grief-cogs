ecea7ae2ac19b07b856166d3e98564a9fbccd3c1
resent <contact@resent.dev>
2023-08-14 16:08:13 -0400

    [view_assets] - fix banner & embed colours

--- view_assets/view_assets.py
+++ view_assets/view_assets.py
@@ -29,7 +29,7 @@ class ViewAssets(commands.Cog):
         embed_list = []
         for name, img_url in img_dict.items():
             if img_url:
-                embed = discord.Embed(colour=discord.Colour.blurple(), title=name)
+                embed = discord.Embed(colour=discord.Colour.dark_theme(), title=name)
                 embed.description = self.IMAGE_HYPERLINK.format(img_url)
                 embed.set_image(url=img_url)
                 embed_list.append(embed)
@@ -50,32 +50,53 @@ class ViewAssets(commands.Cog):
         embed_list = []
         for name, img_url in img_dict.items():
             if img_url:
-                embed = discord.Embed(colour=discord.Colour.blurple(), title=name)
+                embed = discord.Embed(colour=discord.Colour.dark_theme(), title=name)
                 embed.description = self.IMAGE_HYPERLINK.format(img_url)
                 embed.set_image(url=img_url)
                 embed_list.append(embed)
         if not embed_list:
             await ctx.send("This server doesn't have a banner set.")
-            await SimpleMenu(embed_list).start(ctx)
+        await SimpleMenu(embed_list).start(ctx)
         
     @commands.command()
-    async def splash(self, ctx: commands.Context):
+    async def invsplash(self, ctx: commands.Context):
         """Get the server image(s) as embed
 
         If only a server logo exists, that will be displayed.
         Otherwise, a menu including a server banner and splash will be sent."""
         gld: discord.Guild = ctx.guild
         img_dict = {
-            "Server Invite Splash": gld.splash.url if gld.splash else None,
             "Server Discovery Splash": gld.discovery_splash.url if gld.discovery_splash else None,
         }
         embed_list = []
         for name, img_url in img_dict.items():
             if img_url:
-                embed = discord.Embed(colour=discord.Colour.blurple(), title=name)
+                embed = discord.Embed(colour=discord.Colour.dark_theme(), title=name)
                 embed.description = self.IMAGE_HYPERLINK.format(img_url)
                 embed.set_image(url=img_url)
                 embed_list.append(embed)
         if not embed_list:
             await ctx.send("This server doesn't have a discovery or invite splash set.")
+        await SimpleMenu(embed_list).start(ctx)
+
+
+    @commands.command()
+    async def dsplash(self, ctx: commands.Context):
+        """Get the server image(s) as embed
+
+        If only a server logo exists, that will be displayed.
+        Otherwise, a menu including a server banner and splash will be sent."""
+        gld: discord.Guild = ctx.guild
+        img_dict = {
+            "Server Discovery Splash": gld.discovery_splash.url if gld.discovery_splash else None,
+        }
+        embed_list = []
+        for name, img_url in img_dict.items():
+            if img_url:
+                embed = discord.Embed(colour=discord.Colour.dark_theme(), title=name)
+                embed.description = self.IMAGE_HYPERLINK.format(img_url)
+                embed.set_image(url=img_url)
+                embed_list.append(embed)
+        if not embed_list:
+            await ctx.send("No images.")
         await SimpleMenu(embed_list).start(ctx)
