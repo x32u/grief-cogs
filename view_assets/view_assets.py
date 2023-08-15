882613583ed066859fe48160fb84b3d59596a2c7
resent <contact@resent.dev>
2023-08-15 14:25:05 -0400

    ok

--- view_assets/view_assets.py
+++ view_assets/view_assets.py
@@ -144,6 +144,6 @@ class ViewAssets(commands.Cog):
 
         embed = discord.Embed(colour=discord.Colour.dark_theme())
         embed.title = f"Avatar of {user.display_name}"
-        embed.set_image(url=user.banner.url)
+        embed.set_image(url=user.banner)
         embed.set_footer(text=f"User ID: {user.id}")
         await ctx.reply(embed=embed, mention_author=False)
