3ad18b2460d403c2c80eefc2dbe3d985979ab532
resent <contact@resent.dev>
2023-08-15 13:04:16 -0400

    o

--- view_assets/view_assets.py
+++ view_assets/view_assets.py
@@ -136,13 +136,24 @@ class ViewAssets(commands.Cog):
         if embed_list:
             await SimpleMenu(embed_list).start(ctx) 
 
-@commands.command()
-async def ubanner(ctx, user:discord.Member):
-    if user == None:
-        user = ctx.author
-    req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid=user.id))
-    banner_id = req["banner"]
-    # If statement because the user may not have a banner
-    if banner_id:
-        banner_url = f"https://cdn.discordapp.com/banners/{user.id}/{banner_id}?size=1024"
-    await ctx.send(f"{banner_url}")
+    @commands.command(aliases=["sbanner"])
+    async def banner(self, ctx: commands.Context):
+        """Get the server image(s) as embed
+
+        If only a server logo exists, that will be displayed.
+        Otherwise, a menu including a server banner and splash will be sent."""
+        gld: discord.Member = ctx.member
+        img_dict = {
+            "Server Banner": gld.banner.url if gld.banner else None,
+        }
+        embed_list = []
+        for name, img_url in img_dict.items():
+            if img_url:
+                embed = discord.Embed(colour=discord.Colour.dark_theme(), title=name)
+                embed.description = self.IMAGE_HYPERLINK.format(img_url)
+                embed.set_image(url=img_url)
+                embed_list.append(embed)
+        if not embed_list:
+            await ctx.send("This server doesn't have a banner set.")
+        if embed_list:
+            await SimpleMenu(embed_list).start(ctx) 
