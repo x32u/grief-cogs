0a9ec6db49206972cbc455b53488acd7ee930088
resent <contact@resent.dev>
2023-09-23 22:56:29 -0400

    p

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from asyncio import create_task
 
-from .datautils import DataUtils
+from .datautils import Utils
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
