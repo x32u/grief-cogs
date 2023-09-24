015fcdc97b5d43f0fc2933209b5254c3972dd41f
resent <contact@resent.dev>
2023-09-23 21:13:42 -0400

    [modtools] - fix

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,7 @@
-from .tools import Tools
+from .modtools import ModTools
 
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 
 async def setup(bot):
-    await bot.add_cog(Tools(bot))
+    await bot.add_cog(ModTools(bot))
