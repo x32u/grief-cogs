de786de44cb3c0a5017630ab6813d6aeb1759e5b
resent <contact@resent.dev>
2023-09-23 22:09:45 -0400

    p

--- modtools/modtools.py
+++ modtools/modtools.py
@@ -39,10 +39,10 @@ class ModTools(commands.Cog):
             log.info(f"Tools Cog :: Error Occured :: \n{cause}\n")
 
     @commands.guild_only()
-	@commands.command()
-	async def onlinestatus(self, ctx):
-		"""Print how many people are using each type of device."""
-		device = {
+    @commands.command()
+    async def onlinestatus(self, ctx):
+        """Print how many people are using each type of device."""
+        device = {
 			(True, True, True): 0,
 			(False, True, True): 1,
 			(True, False, True): 2,
@@ -52,15 +52,15 @@ class ModTools(commands.Cog):
 			(False, True, False): 6,
 			(False, False, False): 7
 		}
-		store = [0, 0, 0, 0, 0, 0, 0, 0]
-		for m in ctx.guild.members:
-			value = (
+        store = [0, 0, 0, 0, 0, 0, 0, 0]
+        for m in ctx.guild.members:
+            value = (
 				m.desktop_status == discord.Status.offline,
 				m.web_status == discord.Status.offline,
 				m.mobile_status == discord.Status.offline
 			)
-			store[device[value]] += 1
-		msg = (
+            store[device[value]] += 1
+        msg = (
 			f'offline all: {store[0]}'
 			f'\ndesktop only: {store[1]}'
 			f'\nweb only: {store[2]}'
@@ -70,30 +70,30 @@ class ModTools(commands.Cog):
 			f'\ndesktop mobile: {store[6]}'
 			f'\nonline all: {store[7]}'
 		)
-		await ctx.send(f'```py\n{msg}```')
+        await ctx.send(f'```py\n{msg}```')
 
-	@commands.guild_only()
-	@commands.command()
-	async def devices(self, ctx, *, member: discord.Member=None):
-		"""Show what devices a member is using."""
-		if member is None:
-			member = ctx.author
-		d = str(member.desktop_status)
-		m = str(member.mobile_status)
-		w = str(member.web_status)
+    @commands.guild_only()
+    @commands.command()
+    async def devices(self, ctx, *, member: discord.Member=None):
+        """Show what devices a member is using."""
+        if member is None:
+            member = ctx.author
+        d = str(member.desktop_status)
+        m = str(member.mobile_status)
+        w = str(member.web_status)
 		#because it isn't supported in d.py, manually override if streaming
-		if any([isinstance(a, discord.Streaming) for a in member.activities]):
-			d = d if d == 'offline' else 'streaming'
-			m = m if m == 'offline' else 'streaming'
-			w = w if w == 'offline' else 'streaming'
-		status = {
+        if any([isinstance(a, discord.Streaming) for a in member.activities]):
+            d = d if d == 'offline' else 'streaming'
+            m = m if m == 'offline' else 'streaming'
+            w = w if w == 'offline' else 'streaming'
+            status = {
 			'online': '\U0001f7e2',
 			'idle': '\U0001f7e0',
 			'dnd': '\N{LARGE RED CIRCLE}',
 			'offline': '\N{MEDIUM WHITE CIRCLE}',
 			'streaming': '\U0001f7e3'
 		}
-		embed = discord.Embed(
+        embed = discord.Embed(
 			title=f'**{member.display_name}\'s devices:**',
 			description=(
 				f'{status[d]} Desktop\n'
@@ -102,14 +102,14 @@ class ModTools(commands.Cog):
 			),
 			color=await ctx.embed_color()
 		)
-		if discord.version_info.major == 1:
-			embed.set_thumbnail(url=member.avatar_url)
-		else:
-			embed.set_thumbnail(url=member.display_avatar.url)
-		try:
-			await ctx.send(embed=embed)
-		except discord.errors.Forbidden:
-			await ctx.send(
+        if discord.version_info.major == 1:
+            embed.set_thumbnail(url=member.avatar_url)
+        else:
+            embed.set_thumbnail(url=member.display_avatar.url)
+        try:
+            await ctx.send(embed=embed)
+        except discord.errors.Forbidden:
+            await ctx.send(
 				f'{member.display_name}\'s devices:\n'
 				f'{status[d]} Desktop\n'
 				f'{status[m]} Mobile\n'
