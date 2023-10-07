999e71074a7575be6b50e99bb1e03700965749b9
resent <contact@resent.dev>
2023-10-07 10:27:00 -0400

    p

--- customhelp/core/views.py
+++ customhelp/core/views.py
@@ -11,7 +11,6 @@ if TYPE_CHECKING:
 
 LOG = logging.getLogger("grief.customhelp")
 
-
 class ComponentType(enum.IntEnum):
     MENU = 0
     ARROW = 1
@@ -177,9 +176,3 @@ class SelectArrowHelpBar(discord.ui.Select):
             max_values=1,
             options=arrows,
         )
-
-    async def callback(self, interaction):
-        if self.values:
-            if self.values[0] == "Home":
-                await self.view.hmenu.category_react_action(self.view.ctx, interaction, "home")
-            await self.view.hmenu.arrow_emoji_button[self.values[0]](interaction)
