f6affb4d04f0986ac82d8935687c56ab1b8bb2a0
resent <contact@resent.dev>
2023-10-08 15:16:22 -0400

    maybe

--- shell/shell.py
+++ shell/shell.py
@@ -1,16 +1,4 @@
-# Copyright 2018-present Jakub Kuczys (https://github.com/Jackenmen)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 import asyncio
 import asyncio.subprocess as asp
@@ -18,11 +6,11 @@ import contextlib
 import os
 from typing import Any, Dict, List, Literal
 
-from redbot.core import commands
-from redbot.core.bot import Red
-from redbot.core.commands import NoParseOptional as Optional
-from redbot.core.config import Config
-from redbot.core.utils.chat_formatting import inline
+from grief.core import commands
+from grief.core.bot import Red
+from grief.core.commands import NoParseOptional as Optional
+from grief.core.config import Config
+from grief.core.utils.chat_formatting import inline
 
 from .errors import ProcessTerminatedEarly
 from .utils import get_env, send_pages, strip_code_block, wait_for_result
