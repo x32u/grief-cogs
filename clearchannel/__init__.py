43bdfbefb2f251e9bdcdbc191fa5664a9bd6962a
resent <contact@resent.dev>
2023-10-08 15:31:45 -0400

    sigh

--- clearchannel/__init__.py
+++ clearchannel/__init__.py
@@ -1,22 +1,8 @@
 from grief.core import errors  # isort:skip
 import importlib
 import sys
-
-try:
-    import AAA3A_utils
+import AAA3A_utils
 except ModuleNotFoundError:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
-    )
-modules = sorted(
-    [module for module in sys.modules if module.split(".")[0] == "AAA3A_utils"], reverse=True
-)
-for module in modules:
-    try:
-        importlib.reload(sys.modules[module])
-    except ModuleNotFoundError:
-        pass
-del AAA3A_utils
 
 from grief.core.bot import Red  # isort:skip
 from grief.core.utils import get_end_user_data_statement
