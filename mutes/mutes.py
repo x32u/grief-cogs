a37750e65f4caec99d11fb53ac2a435fc69d09fa
resent <contact@resent.dev>
2023-10-13 12:48:27 -0400

    Update mutes.py

--- mutes/mutes.py
+++ mutes/mutes.py
@@ -10,19 +10,19 @@ from datetime import datetime, timedelta, timezone
 from .converters import MuteTime
 from .voicemutes import VoiceMutes
 
-from redbot.core.bot import Red
-from redbot.core import commands, i18n, modlog, Config
-from redbot.core.utils import AsyncIter, bounded_gather, can_user_react_in
-from redbot.core.utils.chat_formatting import (
+from grief.core.bot import Red
+from grief.core import commands, i18n, modlog, Config
+from grief.core.utils import AsyncIter, bounded_gather, can_user_react_in
+from grief.core.utils.chat_formatting import (
     bold,
     humanize_timedelta,
     humanize_list,
     inline,
     pagify,
 )
-from redbot.core.utils.mod import get_audit_reason
-from redbot.core.utils.menus import start_adding_reactions
-from redbot.core.utils.predicates import MessagePredicate, ReactionPredicate
+from grief.core.utils.mod import get_audit_reason
+from grief.core.utils.menus import start_adding_reactions
+from grief.core.utils.predicates import MessagePredicate, ReactionPredicate
 
 T_ = i18n.Translator("Mutes", __file__)
 
