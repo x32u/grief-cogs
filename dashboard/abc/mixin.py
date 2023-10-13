7bd5eef6a8a4b166bd4d55ad0ed5ca62186aa70c
resent <contact@resent.dev>
2023-10-13 15:51:41 -0400

    [dashboard] - make commands bot owner only

--- dashboard/abc/mixin.py
+++ dashboard/abc/mixin.py
@@ -1,6 +1,7 @@
-from grief.core import commands
+from grief.core import commands, checks
 
 
+@checks.is_owner()
 @commands.group(name="dashboard")
 async def dashboard(self, ctx: commands.Context):
     """Group command for controlling the web dashboard for Red."""
