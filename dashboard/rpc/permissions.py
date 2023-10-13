5149204eb5c3beca20f44103960003d6cd8b9a10
resent <contact@resent.dev>
2023-10-13 00:56:10 -0400

    Update permissions.py

--- dashboard/rpc/permissions.py
+++ dashboard/rpc/permissions.py
@@ -2,7 +2,6 @@ from typing import cast
 from html import escape
 
 import discord
-from grief.cogs.permissions.converters import CogOrCommand, GuildUniqueObjectFinder, RuleType
 from grief.core.bot import Red
 from grief.core.commands import commands
 from grief.core.utils import AsyncIter
