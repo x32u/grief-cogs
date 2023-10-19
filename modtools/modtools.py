f1cdb3016a5a7c3ce2f84fd0feddc584822533dc
resent <contact@resent.dev>
2023-10-19 09:49:13 -0400

    [modtools] -

--- modtools/modtools.py
+++ modtools/modtools.py
@@ -584,3 +584,77 @@ class ModTools(commands.Cog):
             key=lambda t: t[0].position,
         )
         return channels, category_channels
+    
+    @checks.mod()
+    @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
+    async def cnick(self, ctx, user: discord.Member, nickname: str, *, reason: Optional[str]):
+        """Forcibly change a user's nickname."""
+        valid_nick_check = self.valid_nickname(nickname=nickname)
+        if not valid_nick_check:
+            return await ctx.send(
+                "That nickname is too long. Keep it under 32 characters, please."
+            )
+        if not reason:
+            reason = ("Nickname force-changed")
+        try:
+            await user.edit(nick=nickname)
+            await ctx.tick()
+        except discord.errors.Forbidden:
+            await ctx.send("Missing permissions.")
+
+    @checks.mod()
+    @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
+    async def freezenick(
+        self,
+        ctx,
+        user: discord.Member,
+        nickname: str,
+        *,
+        reason: Optional[str] = "Nickname frozen.",
+    ):
+        """Freeze a users nickname."""
+        name_check = await self.config.guild(ctx.guild).frozen()
+        for id in name_check:
+            if user.id in id:
+                return await ctx.send("User is already frozen. Unfreeze them first.")
+        valid_nick_check = self.valid_nickname(nickname=nickname)
+        if not valid_nick_check:
+            return await ctx.send("That nickname is too long. Keep it under 32 characters, please")
+
+        try:
+            await user.edit(nick=nickname)
+            await ctx.tick()
+            async with self.config.guild(ctx.guild).frozen() as frozen:
+                frozen.append((user.id, nickname))
+        except discord.errors.Forbidden:
+            await ctx.send("Missing permissions.")
+
+    @checks.mod()
+    @commands.command()
+    async def unfreezenick(self, ctx, user: discord.Member):
+        """Unfreeze a user's nickname."""
+        async with self.config.guild(ctx.guild).frozen() as frozen:
+            for e in frozen:
+                if user.id in e:
+                    frozen.remove(e)
+                    await ctx.tick()
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.nick != after.nick:
+            settings = await self.config.guild(after.guild).frozen()
+            for e in settings:
+                if after.id in e:
+                    if after.nick != e[1]:
+                        try:
+                            await after.edit(nick=e[1], reason="Nickname frozen.")
+                        except discord.errors.Forbidden:
+                            log.info(
+                                f"Missing permissions to change {before.nick} ({before.id}) in {before.guild.id}, removing freeze"
+                            )
+                            async with self.config.guild(after.guild).frozen() as frozen:
+                                for e in frozen:
+                                    if e[0] == before.id:
+                                        frozen.remove(e)
