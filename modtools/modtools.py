75baf744386cf737b2578c98bdadd38e17a978ba
resent <contact@resent.dev>
2023-10-19 12:33:13 -0400

    p

--- modtools/modtools.py
+++ modtools/modtools.py
@@ -593,8 +593,8 @@ class ModTools(commands.Cog):
     @checks.mod()
     @commands.command()
     @checks.bot_has_permissions(manage_nicknames=True)
-    async def nick(self, ctx, user: discord.Member, nickname: str, reason: Optional[str]):
-        """Forcibly change a user's nickname."""
+    async def nick(self, ctx, user: discord.Member, nickname: str=None, reason: Optional[str]):
+        """Change a user's nickname."""
         valid_nick_check = self.valid_nickname(nickname=nickname)
         if not valid_nick_check:
             return await ctx.send(
