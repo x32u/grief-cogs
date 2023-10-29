d27422dda30c242d450f9a5545e07ce8269c3c11
resent <contact@resent.dev>
2023-10-29 10:49:42 -0400

    p

--- cyclestatus/__init__.py
+++ cyclestatus/__init__.py
@@ -5,9 +5,6 @@ from grief.core.bot import Red
 
 from .cycle_status import CycleStatus
 
-with open(pathlib.Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 async def setup(bot: Red):
     await bot.add_cog(CycleStatus(bot))
