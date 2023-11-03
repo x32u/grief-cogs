c556e6017e5e277c5ad2eca7d065a946a94c5a68
resent <contact@resent.dev>
2023-11-03 16:06:19 -0400

    p

--- info/info.py
+++ info/info.py
@@ -176,17 +176,15 @@ class Info(commands.Cog):
         if user == None:user = ctx.author
         if len(user.roles) > 1:role_string = ' '.join([r.mention for r in user.roles][1:])
         date_format = "%a, %d %b %Y %I:%M %p"
-        userbanner = user.banner
         if user.banner == None:
             bannernull = discord.Embed(description=f"**Created:** {user.created_at.strftime(date_format)}\n**Joined:** {user.joined_at.strftime(date_format)}", colour=0x313338)
             bannernull.set_author(name=f"{user.display_name}#{user.discriminator}", url=f"https://discord.com/users/{user.id}", icon_url=f"{user.display_avatar}")
             bannernull.add_field(name="Roles: ` {} `".format(len(user.roles)-1), value=role_string, inline=True)
             bannernull.add_field(name="Misc:", value=f"[**` Avatar `**]({user.display_avatar})\n[**` Profile `**](https://discord.com/users/{user.id})", inline=True)
             bannernull.set_thumbnail(url=f"{user.avatar}")
-            banner_url= user.banner.url
-            iconurl = Button(label="Icon", url=user.avatar.url)
-            profileurl = Button(label="Profile", url=f"https://discord.com/users/{user.id}")
-            banner = Button(label="Banner", url=banner_url)
+            iconurl = Button(label="icon", url=user.avatar.url)
+            profileurl = Button(label="profile", url=f"https://discord.com/users/{user.id}")
+            banner = Button(label="banner", url=user.banner.url)
             view = View()
             view.add_item(iconurl)
             view.add_item(profileurl)
