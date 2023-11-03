b28b654f7c2fd2f8c81672d7efa768392d52c8c2
resent <contact@resent.dev>
2023-11-03 12:49:07 -0400

    p

--- owner/__init__.py
+++ owner/__init__.py
@@ -1,7 +1,33 @@
-from owner.owner import Owner
+import discord
+from discord.utils import get
+from grief.core import commands
+from grief.core.bot import Red
 
-__red_end_user_data_statement__ = "This cog does not store any end user data."
 
+class Owner(commands.Cog):
 
-async def setup(bot):
-    await bot.add_cog(Owner(bot))
+    async def red_delete_data_for_user(self, **kwargs):
+        """
+        Nothing to delete.
+        """
+        return
+
+    def __init__(self, bot: Red):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def dm(self, ctx, user: discord.User, *, message: str):
+        """
+        Dm raw text to a user.
+        """
+        destination = get(self.bot.get_all_members(), id=user.id)
+        if not destination:
+            return await ctx.send(
+                "Invalid ID or user not found. You can only send messages to people I share a server with.",
+            )
+        await destination.send(message)
+        await ctx.send(f"Sent message to {destination}.")
+
+    async def setup(bot):
+        await bot.add_cog(Owner(bot))
