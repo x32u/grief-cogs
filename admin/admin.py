ffd2ed9d7bdf6881722ad514769ba668f469d915
resent <contact@resent.dev>
2023-11-03 14:03:33 -0400

    test

--- admin/admin.py
+++ admin/admin.py
@@ -11,6 +11,15 @@ from grief.core.utils.predicates import MessagePredicate
 
 from .announcer import Announcer
 from .converters import SelfRole
+import typing
+
+try:
+    from emoji import UNICODE_EMOJI_ENGLISH as EMOJI_DATA
+except ImportError:
+    from emoji import EMOJI_DATA
+
+import aiohttp
+
 
 log = logging.getLogger("grief.admin")
 
@@ -65,6 +74,9 @@ RUNNING_ANNOUNCEMENT = _(
     " different announcement please use `{prefix}announce cancel`"
     " first."
 )
+
+ERROR_MESSAGE = _("I attempted to do something that Discord denied me permissions for. Your command failed to successfully complete.\n{error}")
+
 _ = T_
 
 
@@ -86,6 +98,18 @@ class Admin(commands.Cog):
 
         self.__current_announcer = None
 
+class EmojiOrUrlConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: str):
+        try:
+            return await discord.ext.commands.converter.CONVERTER_MAPPING[discord.Emoji]().convert(
+                ctx, argument
+            )
+        except commands.BadArgument:
+            pass
+        if argument.startswith("<") and argument.endswith(">"):
+            argument = argument[1:-1]
+        return argument
+    
     async def cog_load(self) -> None:
         await self.handle_migrations()
 
@@ -325,6 +349,54 @@ class Admin(commands.Cog):
         else:
             log.info(reason)
             await ctx.send(_("Done."))
+
+    @commands.guild_only()
+    @commands.admin_or_permissions(manage_roles=True)
+    @commands.bot_has_permissions(manage_roles=True)
+    @editrole.command(name="icon")
+    async def editrole_icon(
+        self, ctx: commands.Context, role: discord.Role, display_icon: typing.Optional[EmojiOrUrlConverter] = None
+    ) -> None:
+        """Edit role display icon.
+
+        `display_icon` can a Unicode emoji, a custom emoji or an url. You can also upload an attachment.
+        """
+        if "ROLE_ICONS" not in ctx.guild.features:
+            raise commands.UserFeedbackCheckFailure(_("This server doesn't have `ROLE_ICONS` feature. This server needs more boosts to perform this action."))
+        await self.check_role(ctx, role)
+        if len(ctx.message.attachments) > 0:
+            display_icon = await ctx.message.attachments[0].read()  # Read an optional attachment.
+        elif display_icon is not None:
+            if isinstance(display_icon, discord.Emoji):
+                # emoji_url = f"https://cdn.discordapp.com/emojis/{display_icon.id}.png"
+                # async with aiohttp.ClientSession() as session:
+                #     async with session.get(emoji_url) as r:
+                #         display_icon = await r.read()  # Get emoji data.
+                display_icon = await display_icon.read()
+            elif display_icon.strip("\N{VARIATION SELECTOR-16}") in EMOJI_DATA:
+                display_icon = display_icon
+            else:
+                url = display_icon
+                async with aiohttp.ClientSession() as session:
+                    try:
+                        async with session.get(url) as r:
+                            display_icon = await r.read()  # Get URL data.
+                    except aiohttp.InvalidURL:
+                        return await ctx.send("That URL is invalid.")
+                    except aiohttp.ClientError:
+                        return await ctx.send("Something went wrong while trying to get the image.")
+        else:
+            await ctx.send_help()  # Send the command help if no attachment, no Unicode/custom emoji and no URL.
+            return
+        try:
+            await role.edit(
+                display_icon=display_icon,
+                reason=f"{ctx.author} ({ctx.author.id}) has edited the role {role.name} ({role.id}).",
+            )
+        except discord.HTTPException as e:
+            raise commands.UserFeedbackCheckFailure(
+                _(ERROR_MESSAGE).format(error=box(e, lang="py"))
+            )
 
     @commands.group(invoke_without_command=True)
     @commands.is_owner()
