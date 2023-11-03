ce12f4f035e3861e2b4caf071b8d8bf3008c84f1
resent <contact@resent.dev>
2023-11-03 14:34:45 -0400

    p

--- info/info.py
+++ info/info.py
@@ -1094,7 +1094,7 @@ class Info(commands.Cog):
     @commands.command()
     async def inviteinfo(self, ctx: commands.Context, code: str): 
         invite_code = code
-        data = await self.bot.session.json(DISCORD_API_LINK + invite_code, proxy=self.bot.proxy_url, ssl=False)
+        data = await self.bot.session.json(DISCORD_API_LINK + invite_code)
         name = data["guild"]["name"]
         id = data['guild']['id']
         description = data["guild"]["description"]
