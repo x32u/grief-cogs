c6a9c0c42e1e79414507abe3fdc150c18093f21c
resent <contact@resent.dev>
2023-11-03 21:13:35 -0400

    p

--- info/info.py
+++ info/info.py
@@ -1067,9 +1067,10 @@ class Info(commands.Cog):
 
         await ctx.send(embed=data)
 
-    @commands.command(description="fun")
+    @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def uwu(self, ctx, *, message):
+      """Uwuify a message."""
       if message == None:
             embed = discord.Embed(description=f"{ctx.author.mention} what do you want me to uwuify?", color = 0x313338)
             await ctx.reply(embed=embed, mention_author=False)
