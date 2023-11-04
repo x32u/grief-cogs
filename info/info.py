493f01bb4f98e7cc3a83e9fb2140d4375f03b290
resent <contact@resent.dev>
2023-11-03 23:31:22 -0400

    p

--- info/info.py
+++ info/info.py
@@ -181,15 +181,11 @@ class Info(commands.Cog):
         if user == None:user = ctx.author,
         if len(user.roles) > 1:role_string = ' '.join([r.mention for r in user.roles][1:])
         date_format = "%a, %d %b %Y %I:%M %p"
-        
-        
         if user.banner == None:
             bannernull = discord.Embed(description=f"**Created:** {user.created_at.strftime(date_format)}\n**Joined:** {user.joined_at.strftime(date_format)}", colour=0x313338)
             bannernull.set_author(name=f"{user.display_name}", url=f"https://discord.com/users/{user.id}", icon_url=f"{user.display_avatar}")
             bannernull.add_field(name="Roles: {}".format(len(user.roles)-1), value=role_string, inline=True)
             bannernull.set_thumbnail(url=f"{user.avatar}")
-            
-            
             banner_url = Button(label="banner", url=user.banner.url)
             icon_url = Button(label="icon", url=user.avatar.url)
             profile_url = Button(label="profile", url=f"https://discord.com/users/{user.id}")
