f2de1bf08550d367d1ef159d81bd0e406ef95574
resent <contact@resent.dev>
2023-11-04 11:23:07 -0400

    [cogs] - remove info file

--- cleanup/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cleanup import Cleanup
-from grief.core.bot import Red
-
-
-async def setup(bot: Red) -> None:
-    await bot.add_cog(Cleanup(bot))
--- modlog/__init__.py
+++
@@ -1,6 +0,0 @@
-from grief.core.bot import Red
-from .modlog import ModLog
-
-
-async def setup(bot: Red) -> None:
-    await bot.add_cog(ModLog(bot))
