33b5c0e57642b7b248058d061fd915535430fba3
resent <contact@resent.dev>
2023-11-03 23:26:41 -0400

    p

--- info/info.py
+++ info/info.py
@@ -178,8 +178,8 @@ class Info(commands.Cog):
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def userinfo(self, ctx: commands.Context, *, user:discord.Member = None):
         """Grab information on a user."""
-        if member == None:member = ctx.author
         user = await self.bot.fetch_user(member.id)
+        if member == None:member = ctx.author
         if len(user.roles) > 1:role_string = ' '.join([r.mention for r in user.roles][1:])
         date_format = "%a, %d %b %Y %I:%M %p"
         if user.banner == None:
