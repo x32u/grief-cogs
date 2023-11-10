f16657bf853c6040da28abc30797920370df3e7c
resent <contact@resent.dev>
2023-11-10 00:37:13 -0500

    [birthday] - update permissions

--- birthday/__init__.py
+++ birthday/__init__.py
@@ -8,9 +8,7 @@ from grief.core.bot import Red
 
 from . import vexutils
 from .birthday import Birthday
-from .vexutils.meta import out_of_date_check
 
 async def setup(bot: Red) -> None:
     cog = Birthday(bot)
-    await out_of_date_check("birthday", cog.__version__)
     await bot.add_cog(cog)
