b2a493c55d7bd08ed466a9825e66f51d25dcc629
resent <contact@resent.dev>
2023-11-10 17:31:15 -0500

    [mod] - ..

--- mod/names.py
+++ mod/names.py
@@ -290,7 +290,7 @@ class ModInfo(MixinMeta):
         data.set_author(name=f"{statusemoji} {name}", url=avatar)
         data.set_thumbnail(url=avatar)
 
-        await ctx.send(embed=data)
+        await ctx.reply(embed=data, mention_author=False)
 
     @commands.command()
     async def names(self, ctx: commands.Context, *, member: discord.Member):
