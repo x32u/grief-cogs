df008796eabf16abdaac1e7ce1c3d23b97816e9e
resent <contact@resent.dev>
2023-11-09 22:53:55 -0500

    [linkquoter] - update permissions

--- linkquoter/linkquoter.py
+++ linkquoter/linkquoter.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2020-present phenom4n4n
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 import asyncio
 import logging
@@ -32,7 +9,7 @@ from grief.core import Config, checks, commands
 
 from .converters import LinkToMessage
 
-log = logging.getLogger("red.phenom4n4n.linkquoter")
+log = logging.getLogger("grief.linkquoter")
 
 COOLDOWN = (3, 10, commands.BucketType.channel)
 
@@ -255,7 +232,7 @@ class LinkQuoter(commands.Cog):
             embed = await self.message_to_embed(message_link, invoke_guild=ctx.guild)
             await ctx.send(embed=embed)
 
-    @commands.admin_or_permissions(manage_guild=True)
+    @commands.has_permissions(manage_guild=True)
     @commands.guild_only()
     @commands.group()
     async def linkquoteset(self, ctx: commands.Context):
@@ -327,7 +304,6 @@ class LinkQuoter(commands.Cog):
             await ctx.send("This server is no longer opted in to cross-server quoting.")
 
     @commands.check(webhook_check)
-    @checks.bot_has_permissions(manage_webhooks=True)
     @linkquoteset.command(name="webhook")
     async def linkquoteset_webhook(self, ctx, true_or_false: bool = None):
         """
