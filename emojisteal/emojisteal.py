7f91e5ca22fc4d19f85a2c0bd788b40dec20a750
resent <contact@resent.dev>
2023-11-12 16:36:16 -0500

    [emojisteal] - test

--- emojisteal/emojisteal.py
+++ emojisteal/emojisteal.py
@@ -97,7 +97,6 @@ class EmojiSteal(commands.Cog):
         return emojis
 
     @commands.command(name="enlarge", invoke_without_command=True)
-    @commands.has_permissions(manage_expressions=True)
     async def steal_command(self, ctx: commands.Context):
         """Enlarges the emojis and stickers of the message you reply to. Can also upload them with ;steal upload."""
         if not (emojis := await self.steal_ctx(ctx)):
