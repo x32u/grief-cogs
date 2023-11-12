1aeefe9cc33604b94484f11ea64a4e32b7ef4c88
resent <contact@resent.dev>
2023-11-12 17:36:28 -0500

    [owner] - fix enbed reply mentioning

--- owner/__init__.py
+++ owner/__init__.py
@@ -39,8 +39,8 @@ class Owner(commands.Cog):
     async def ping(self, ctx):
         """View bot latency."""
         start = time.monotonic()
-        ref = ctx.message.to_reference(fail_if_not_exists=False,)
-        message = await ctx.reply("Pinging...", reference=ref)
+        ref = ctx.message.to_reference(fail_if_not_exists=False, mention_author=False)
+        message = await ctx.send("Pinging...", reference=ref)
         end = time.monotonic()
         totalPing = round((end - start) * 1000, 2)
         e = discord.Embed(
