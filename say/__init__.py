0502e500cd8d34b6128e8f4cdf7c481730eedbbd
resent <contact@resent.dev>
2023-11-16 22:10:11 -0500

    p

--- say/__init__.py
+++ say/__init__.py
@@ -7,24 +7,12 @@ from .say import Say
 from typing import TYPE_CHECKING
 from discord import app_commands
 from grief.core.errors import CogLoadError
-from laggron_utils import init_logger
 
 if TYPE_CHECKING:
     from grief.core.bot import Red
 
-if not importlib.util.find_spec("laggron_utils"):
-    raise CogLoadError(
-        "You need the `laggron_utils` package for any cog from Laggron's Dumb Cogs. "
-        "Use the command `[p]pipinstall git+https://github.com/retke/Laggron-utils.git` "
-        "or type `pip3 install -U git+https://github.com/retke/Laggron-utils.git` in the "
-        "terminal to install the library."
-    )
-
-log = logging.getLogger("red.laggron.say")
+log = logging.getLogger("grief.say")
 
 
 async def setup(bot: "Red"):
-    init_logger(log, "Say")
-    n = Say(bot)
-    await bot.add_cog(n)
-    log.debug("Cog successfully loaded on the instance.")
+    await bot.add_cog(Say)
