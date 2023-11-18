7b272178e8e12ee1809d73d5f03994b81f3eff2a
resent <contact@resent.dev>
2023-11-17 19:38:42 -0500

    p

--- rolesbuttons/__init__.py
+++ rolesbuttons/__init__.py
@@ -1,7 +1,6 @@
 from grief.core import errors  # isort:skip
 import importlib
 import sys
-import discord
 
 try:
     import AAA3A_utils
@@ -18,6 +17,21 @@ for module in modules:
     except ModuleNotFoundError:
         pass
 del AAA3A_utils
+# import AAA3A_utils
+# import json
+# import os
+# __version__ = AAA3A_utils.__version__
+# with open(os.path.join(os.path.dirname(__file__), "utils_version.json"), mode="r") as f:
+#     data = json.load(f)
+# needed_utils_version = data["needed_utils_version"]
+# if __version__ > needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo."
+#     )
+# elif __version__ < needed_utils_version:
+#     raise errors.CogLoadError(
+#         "The needed utils to run the cog has a lower version than the one supported by this version of the cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot isn't necessary."
+#     )
 
 from grief.core.bot import Red  # isort:skip
 from grief.core.utils import get_end_user_data_statement
@@ -29,3 +43,4 @@ __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
 async def setup(bot: Red) -> None:
     cog = RolesButtons(bot)
+    await bot.add_cog(cog)
