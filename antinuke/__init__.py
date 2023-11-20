2435fb158e302e5fa3885034805d8820724b935c
resent <contact@resent.dev>
2023-11-20 12:13:14 -0500

    [antinuke] - add cog

--- antinuke/__init__.py
+++ antinuke/__init__.py
@@ -2,10 +2,10 @@ import asyncio
 
 from grief.core import VersionInfo, version_info
 
-from .nonuke import NoNuke
+from .antinuke import AntiNuke
 
 async def setup(bot):
-    cog = NoNuke(bot)
+    cog = AntiNuke(bot)
     if version_info >= VersionInfo.from_str("3.5.0"):
         await bot.add_cog(cog)
     else:
