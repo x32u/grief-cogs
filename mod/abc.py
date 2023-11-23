e619a0d9ec2798975252a11e1f15a41d50be426c
resent <contact@resent.dev>
2023-11-23 00:05:43 -0500

    [mod] - move mutes

--- mod/abc.py
+++ mod/abc.py
@@ -32,16 +32,3 @@ class MixinMeta(ABC):
         ctx: commands.Context, user_voice_state: Optional[discord.VoiceState], **perms: bool
     ) -> bool:
         raise NotImplementedError()
-
-    @staticmethod
-    @abstractmethod
-    async def _send_dm_notification(
-        self,
-        user: Union[discord.User, discord.Member],
-        moderator: Optional[Union[discord.User, discord.Member]],
-        guild: discord.Guild,
-        mute_type: str,
-        reason: Optional[str],
-        duration=None,
-    ):
-        raise NotImplementedError()
