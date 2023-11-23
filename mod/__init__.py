cd945170a88961a7b81687d00ddcb96b36e1b4d7
resent <contact@resent.dev>
2023-11-22 23:44:28 -0500

    [mod] - move mutes

--- mod/__init__.py
+++ mod/__init__.py
@@ -1,6 +1,9 @@
 from grief.core.bot import Red
 from .mod import Mod
+from .mutes import Mutes
 
 
 async def setup(bot: Red) -> None:
+    cog = Mod(bot)
     await bot.add_cog(Mod(bot))
+    cog.create_init_task()
