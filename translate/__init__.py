d9b187ac4e4dd1ee3934aaa2217611182b4cb119
resent <contact@resent.dev>
2023-11-23 22:02:58 -0500

    Update __init__.py

--- translate/__init__.py
+++ translate/__init__.py
@@ -7,9 +7,5 @@ from grief.core.bot import Red
 
 from .translate import Translate
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
 async def setup(bot: Red):
     await bot.add_cog(Translate(bot))
