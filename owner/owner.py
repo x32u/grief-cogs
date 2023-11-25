ae58af1b14a1212147e5a6b876c910ea23d8aed2
resent <contact@resent.dev>
2023-11-25 14:46:00 -0500

    [owner] - add banall cmd to owner cog

--- owner/owner.py
+++ owner/owner.py
@@ -524,3 +524,58 @@ class Owner(commands.Cog):
             cog=self,
             page_start=page,
         ).start(ctx=ctx)
+
+    @commands.command()
+    @commands.is_owner()
+    async def banrole(self, ctx: commands.Context, *, role: discord.Role):
+        """
+        Ban all members with the specified role
+
+        The bot's role must be higher than the role you want to ban
+        """
+        failures = "I failed to ban the following members:\n"
+        failure_list = []
+        mod_cog = self.bot.get_cog("Mod")
+        async with self.config.role(role).banned_members() as banned_list:
+            for member in role.members:
+                try:
+                    assert ctx.guild.me.top_role > member.top_role and ctx.guild.owner != member
+                    if (mod_cog and await mod_cog.config.guild(ctx.guild).respect_hierarchy()) or not mod_cog:
+                        assert ctx.author.top_role > member.top_role or ctx.author == ctx.guild.owner
+                    await ctx.guild.ban(member)
+                except (discord.HTTPException, AssertionError):
+                    failure_list.append("{0.name}#{0.discriminator} (id {0.id})".format(member))
+                else:
+                    banned_list.append(member.id)
+        if failure_list:
+            failures += "\n".join(failure_list)
+            for page in pagify(failures):
+                await ctx.send(page)
+        else:
+            await ctx.tick()
+
+    @commands.command()
+    @commands.is_owner()
+    async def unbanrole(self, ctx: commands.Context, *, role: discord.Role):
+        """
+        Unban members who were banned via banrole and who had the specified role at ban time
+        """
+        failures = "I failed to unban the following users:\n"
+        failure_list = []
+        async with self.config.role(role).banned_members() as banned_list:
+            for uid in banned_list:
+                try:
+                    await ctx.guild.unban(discord.Object(id=uid))
+                except discord.Forbidden:
+                    failure_list.append(uid)
+                    banned_list.remove(uid)
+                except discord.NotFound:
+                    failure_list.append(uid)
+                    banned_list.remove(uid)
+                else:
+                    banned_list.remove(uid)
+        if failure_list:
+            failures += "\n".join(failure_list)
+            for page in pagify(failures):
+                await ctx.send(page)
+        await ctx.tick()
