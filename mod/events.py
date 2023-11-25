bd9f49d14c55d2bdb4f5a7ab2458463f82276b8c
resent <contact@resent.dev>
2023-11-25 11:46:22 -0500

    [mod] - add names/comment it out

--- mod/events.py
+++ mod/events.py
@@ -132,29 +132,68 @@ class Events(MixinMeta):
                 return True
         return False
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        author = message.author
-        if message.guild is None or self.bot.user == author:
-            return
+    #@commands.Cog.listener()
+    #async def on_message(self, message):
+     #   author = message.author
+      #  if message.guild is None or self.bot.user == author:
+       #     return
 
-        if await self.bot.cog_disabled_in_guild(self, message.guild):
-            return
+       # if await self.bot.cog_disabled_in_guild(self, message.guild):
+        #    return
 
-        valid_user = isinstance(author, discord.Member) and not author.bot
-        if not valid_user:
-            return
+       # valid_user = isinstance(author, discord.Member) and not author.bot
+        #if not valid_user:
+         #   return
 
         #  Bots and mods or superior are ignored from the filter
-        mod_or_superior = await is_mod_or_superior(self.bot, obj=author)
-        if mod_or_superior:
-            return
+        # mod_or_superior = await is_mod_or_superior(self.bot, obj=author)
+        # if mod_or_superior:
+         #   return
         # As are anyone configured to be
-        if await self.bot.is_automod_immune(message):
-            return
+        # if await self.bot.is_automod_immune(message):
+          #  return
 
-        await i18n.set_contextual_locales_from_guild(self.bot, message.guild)
+        # await i18n.set_contextual_locales_from_guild(self.bot, message.guild)
 
-        deleted = await self.check_duplicates(message)
-        if not deleted:
-            await self.check_mention_spam(message)
+        # deleted = await self.check_duplicates(message)
+        # if not deleted:
+          #  await self.check_mention_spam(message)
+
+    # @staticmethod
+    # def _update_past_names(name: str, name_list: List[Optional[str]]) -> None:
+       # while None in name_list:  # clean out null entries from a bug
+        #     name_list.remove(None)
+        # if name in name_list:
+            # Ensure order is maintained without duplicates occurring
+         #    name_list.remove(name)
+        # name_list.append(name)
+        # while len(name_list) > 20:
+           # name_list.pop(0)
+
+   # @commands.Cog.listener()
+   # async def on_user_update(self, before: discord.User, after: discord.User):
+    #    if before.name != after.name:
+     #       track_all_names = await self.config.track_all_names()
+      #      if not track_all_names:
+         #       return
+         #   async with self.config.user(before).past_names() as name_list:
+          #      self._update_past_names(before.name, name_list)
+        #if before.display_name != after.display_name:
+          #  track_all_names = await self.config.track_all_names()
+           # if not track_all_names:
+            #    return
+            #async with self.config.user(before).past_display_names() as name_list:
+             #   self._update_past_names(before.display_name, name_list)
+
+  #  @commands.Cog.listener()
+   # async def on_member_update(self, before: discord.Member, after: discord.Member):
+   #     if before.nick != after.nick and before.nick is not None:
+    #        guild = after.guild
+     #       if (not guild) or await self.bot.cog_disabled_in_guild(self, guild):
+      #          return
+       #     track_all_names = await self.config.track_all_names()
+        #    track_nicknames = await self.config.guild(guild).track_nicknames()
+         #   if (not track_all_names) or (not track_nicknames):
+           #     return
+            #async with self.config.member(before).past_nicks() as nick_list:
+             #   self._update_past_names(before.nick, nick_list)
