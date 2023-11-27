e2d3d58cd3bc2d373c3845c4e95226aacdf148b9
resent <contact@resent.dev>
2023-11-27 17:40:55 -0500

    [info] - make embeds consistant

--- info/info.py
+++ info/info.py
@@ -93,6 +93,7 @@ class Info(commands.Cog):
             button1 = Button(label="avatar", url=avatar_url)
             e = discord.Embed(color=0x313338, url=user.avatar.url)
             e.set_author(name=f"{member.display_name}", icon_url=f"{member.avatar}", url=f"https://discord.com/users/{member.id}")
+            e.title = f"Avatar of {user.display_name}"
             e.set_image(url=avatar_url)
             view = View()
             view.add_item(button1)
@@ -113,10 +114,7 @@ class Info(commands.Cog):
             embed.set_author(name=f"{user.display_name}", icon_url=f"{user.guild_avatar}", url=f"https://discord.com/users/{user.id}")
             embed.title = f"Server avatar of {user.display_name}"
             embed.set_image(url=gld_avatar_url)
-            button1 = Button(label="server avatar", url=user.guild_avatar.url)
-            view = View()
-            view.add_item(button1)
-            await ctx.reply(embed=embed, view=view, mention_author=False)
+            await ctx.reply(embed=embed, mention_author=False)
     
     @commands.command(aliases=["sicon", "si", "sico", "savi"])
     @commands.cooldown(1, 3, commands.BucketType.user, )
