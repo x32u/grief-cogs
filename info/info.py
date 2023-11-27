c6e64f7590c4df5d895ecdf7eced28f5571b61e6
resent <contact@resent.dev>
2023-11-27 16:11:03 -0500

    p

--- info/info.py
+++ info/info.py
@@ -1314,17 +1314,17 @@ class Info(commands.Cog):
             urls += f"[**icon**]({icon_url}), "
             embed.set_thumbnail(url=icon_url)
         if invite.guild.banner:
-            banner_url = yarl.URL(str(invite.guild.banner_url))
+            banner_url = yarl.URL(str(invite.guild.banner))
             if "a_" in banner_url.path:
                 banner_url = str(banner_url).replace("webp", "gif")
             urls += f"[**banner**]({banner_url}), "
-            lookup = await (str(invite.guild.banner_url))
+            lookup = await (str(invite.guild.banner))
             if lookup:
                 embed.color = lookup.dominant.decimal
             embed.set_image(url=str(banner_url))
 
         if invite.guild.splash:
-            urls += f"[**splash**]({invite.guild.splash_url}), "
+            urls += f"[**splash**]({invite.guild.splash}), "
         if len(urls) > 0:
             embed.add_field(name="**assets**", value=urls[:-2], inline=False)
         await ctx.send(embed=embed)
