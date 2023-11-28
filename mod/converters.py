5be88c9304d2a5d115a6d017348e81fde3d1daf5
resent <contact@resent.dev>
2023-11-28 11:27:45 -0500

    p

--- mod/converters.py
+++ mod/converters.py
@@ -15,6 +15,22 @@ from typing import Union, Dict
 from datetime import timedelta
 
 from discord.ext.commands.converter import Converter
+from re import Pattern
+from typing import Union
+
+import discord
+import regex as re
+import unidecode
+from discord.ext.commands.converter import Converter
+from discord.ext.commands.errors import BadArgument
+from grief.core.commands import BadArgument, MemberConverter
+from rapidfuzz import process
+
+IMAGE_LINKS: Pattern = re.compile(r"(https?:\/\/[^\"\'\s]*\.(?:png|jpg|jpeg|webp|gif|png|svg)(\?size=[0-9]*)?)", flags=re.I)
+EMOJI_REGEX: Pattern = re.compile(r"(<(a)?:[a-zA-Z0-9\_]+:([0-9]+)>)")
+MENTION_REGEX: Pattern = re.compile(r"<@!?([0-9]+)>")
+ID_REGEX: Pattern = re.compile(r"[0-9]{17,}")
+VIDEO_LINKS: Pattern = re.compile(r"(https?:\/\/[^\"\'\s]*\.(?:png|jpg|jpeg|mov|mp4|webv|webp|gif|png|svg)(\?size=[0-9]*)?)", flags=re.I)
 
 
 class ChannelToggle(commands.Converter):
@@ -94,3 +110,60 @@ class LockableRole(FuzzyRole):
                 f"You do not have permission to edit **{role}**'s permissions."
             )
         return role
+
+
+class ImageFinder(Converter):
+    """This is a class to convert notsobots image searching capabilities into a
+    more general converter class.
+    """
+
+    async def convert(self, ctx: commands.Context, argument: str) -> list[Union[discord.Asset, str]]:
+        attachments = ctx.message.attachments
+        mentions = MENTION_REGEX.finditer(argument)
+        matches = IMAGE_LINKS.finditer(argument)
+        emojis = EMOJI_REGEX.finditer(argument)
+        ids = ID_REGEX.finditer(argument)
+        urls = []
+        if matches:
+            urls.extend(match.group(1) for match in matches)
+        if emojis:
+            for emoji in emojis:
+                ext = "gif" if emoji.group(2) else "png"
+                url = f"https://cdn.discordapp.com/emojis/{emoji.group(3)}.{ext}?v=1"
+                urls.append(url)
+        if mentions:
+            for mention in mentions:
+                user = ctx.guild.get_member(int(mention.group(1)))
+                if user.is_avatar_animated():
+                    urls.append(user.avatar_url_as(format="gif"))
+                else:
+                    urls.append(user.avatar_url_as(format="png"))
+        if not urls and ids:
+            for possible_id in ids:
+                if user := ctx.guild.get_member(int(possible_id.group(0))):
+                    if user.is_avatar_animated():
+                        urls.append(user.avatar_url_as(format="gif"))
+                    else:
+                        urls.append(user.avatar_url_as(format="png"))
+        if attachments:
+            urls.extend(attachment.url for attachment in attachments)
+        if not urls:
+            for m in ctx.guild.members:
+                m: discord.Member
+                await checkpoint()
+                if argument.lower() in unidecode.unidecode(m.display_name.lower()):
+                    # display_name so we can get the nick of the user first
+                    # without being NoneType and then check username if that matches
+                    # what we're expecting
+                    urls.append(m.avatar_url_as(format="png"))
+                    continue
+                if argument.lower() in unidecode.unidecode(m.name.lower()):
+                    urls.append(m.avatar_url_as(format="png"))
+                    continue
+        if not urls:
+            urls = await self.search_for_images(ctx)
+
+        if not urls:
+            msg = "No images provided."
+            raise BadArgument(msg)
+        return urls
