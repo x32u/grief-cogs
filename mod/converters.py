b3674ddea6ff761536d4b085a3d62f8cd386c973
resent <contact@resent.dev>
2023-11-28 13:04:57 -0500

    p

--- mod/converters.py
+++ mod/converters.py
@@ -27,12 +27,6 @@ from discord.ext.commands.errors import BadArgument
 from grief.core.commands import BadArgument, MemberConverter
 from rapidfuzz import process
 
-import arrow
-import asyncpg
-import discord
-import yarl
-from yarl import URL
-
 IMAGE_LINKS: Pattern = re.compile(r"(https?:\/\/[^\"\'\s]*\.(?:png|jpg|jpeg|webp|gif|png|svg)(\?size=[0-9]*)?)", flags=re.I)
 EMOJI_REGEX: Pattern = re.compile(r"(<(a)?:[a-zA-Z0-9\_]+:([0-9]+)>)")
 MENTION_REGEX: Pattern = re.compile(r"<@!?([0-9]+)>")
