748d27518c1235674229086ea3608743d568f0c2
resent <contact@resent.dev>
2023-11-29 10:39:27 -0500

    [mod] - update embeds for timeout

--- mod/mod.py
+++ mod/mod.py
@@ -976,7 +976,7 @@ class Mod(
         return overwrite, valid_perms, invalid, not_allowed
     
 
-    @commands.command(aliases=["tt"])
+    @commands.command(aliases=["t"])
     @commands.guild_only()
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.has_permissions(moderate_members=True)
@@ -987,19 +987,19 @@ class Mod(
         timestamp = int(datetime.datetime.timestamp(utcnow() + time))
         if isinstance(member, discord.Member):
             if member.is_timed_out():
-                embed = discord.Embed(description=f"> <:grief_x:1107472962333978655> {member.mention} is already timed out.", color=0x313338)
+                embed = discord.Embed(description=f"> {member.mention} is already timed out.", color=0x313338)
                 return await ctx.reply(embed=embed, mention_author=False)
             if not await is_allowed_by_hierarchy(ctx.bot, ctx.author, member):
-                embed = discord.Embed(description=f"> <:grief_x:1107472962333978655> You cannot timeout this user due to hierarchy.", color=0x313338)
+                embed = discord.Embed(description=f"> You cannot timeout this user due to hierarchy.", color=0x313338)
                 return await ctx.reply(embed=embed, mention_author=False)
             if ctx.channel.permissions_for(member).administrator:
-                embed = discord.Embed(description=f"> <:grief_x:1107472962333978655> You can't timeout an administrator.", color=0x313338)
+                embed = discord.Embed(description=f"> You can't timeout an administrator.", color=0x313338)
                 return await ctx.reply(embed=embed, mention_author=False)
             await self.timeout_user(ctx, member, time, reason)
-            embed = discord.Embed(description=f"> <:grief_check:1107472942830456892> {member.mention} has been timed out until <t:{timestamp}:f>. ", color=0x313338)
+            embed = discord.Embed(description=f"> {member.mention} has been timed out until <t:{timestamp}:f>.", color=0x313338)
             await ctx.reply(embed=embed, mention_author=False)
 
-    @commands.command(aliases=["utt"])
+    @commands.command(aliases=["ut"])
     @commands.guild_only()
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.has_permissions(moderate_members=True)
@@ -1007,10 +1007,10 @@ class Mod(
         """Untimeout users."""
         if isinstance(member, discord.Member):
                 if not member.is_timed_out():
-                    embed = discord.Embed(description=f"> <:grief_x:1107472962333978655> {member.mention} is not timed out.", color=0x313338)
+                    embed = discord.Embed(description=f"> {member.mention} is not timed out.", color=0x313338)
                     return await ctx.reply(embed=embed, mention_author=False)
                 await self.timeout_user(ctx, member, None, reason)
-        embed = discord.Embed(description=f"> <:grief_check:1107472942830456892> Removed the timeout for {member.mention}.", color=0x313338)
+        embed = discord.Embed(description=f"> Removed the timeout for {member.mention}.", color=0x313338)
         await ctx.reply(embed=embed, mention_author=False)
         
 async def is_allowed_by_hierarchy(
