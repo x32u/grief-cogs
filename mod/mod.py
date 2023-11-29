aec7a586d01906dad90683e0b62e5a1adae04a1a
resent <contact@resent.dev>
2023-11-29 10:13:14 -0500

    p

--- mod/mod.py
+++ mod/mod.py
@@ -1000,7 +1000,8 @@ class Mod(
         timestamp = int(datetime.datetime.timestamp(utcnow() + time))
         if isinstance(member, discord.Member):
             if member.is_timed_out():
-                return await ctx.reply(embed = discord.Embed(description=f"> {member.mention} is already timed out.", embed=embed, mention_author=False, color=0x313338))
+                embed = discord.Embed(description=f"> {member.mention} is already timed out.", color=0x313338)
+                return await ctx.reply(embed=embed, mention_author=False)
             if not await is_allowed_by_hierarchy(ctx.bot, ctx.author, member):
                 return await ctx.send("You cannot timeout this user due to hierarchy.")
             if ctx.channel.permissions_for(member).administrator:
