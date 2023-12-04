a2ae1bef0985b0a80e3d9a78cd500c97c37644a0
resent <contact@resent.dev>
2023-12-04 17:35:44 -0500

    ughh

--- vanity/__init__.py
+++ vanity/__init__.py
@@ -163,8 +163,8 @@ class Vanity(commands.Cog):
         """Toggle vanity checker for current server on/off. Do not use "/"."""
         await self.config.guild(ctx.guild).toggled.set(on)
         await self.config.guild(ctx.guild).vanity.set(vanity)
-        if "VANITY_URL" not in ctx.guild.features:
-            await ctx.send("This guild does not currently have a vanity URL. This feature is for level 3 vanity servers only.")
+        if ctx.guild.premium_tier != 3:
+            return await ctx.send("This feature is reserved or level 3 servers.")
         if "VANITY_URL" in ctx.guild.features:
             embed = discord.Embed(description=f"> Vanity status tracking for current server is now {'on' if on else 'off'} and set to {vanity}.", color=0x313338)
             return await ctx.reply(embed=embed, mention_author=False)
