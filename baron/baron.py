adedf261dd18e14c2700eef550bb84fc7b48fb82
resent <contact@resent.dev>
2023-12-12 16:08:48 -0500

    test

--- baron/baron.py
+++ baron/baron.py
@@ -702,7 +702,6 @@ class Baron(commands.Cog):
         if data["min_members"] and guild.member_count < data["min_members"]:
             await self.notify_guild(guild, f"grief is whitelist only if you would like your server whitelisted, please join https://discord.gg/seer and check out https://discord.com/channels/926754520682336297/1173290858339115148"),
             await guild.leave()
-
-        if data["min_members"] and guild.member_count > data["min_members"]:
+        else:
             await self.notify_guild(guild, f"Hello, I am grief. I am a free mutlipurpose Discord bot that features vanity rewards, TikTok/YouTube reposting, and pretty much everything you'd need. Below you can find my links:\n\n Community server — https://discord.gg/yor\n\n Support Server — https://discord.gg/seer\n\n Commands — <https://grief.cloud/commands>\n\n Docs — <https://docs.grief.cloud>"),
             return
