82ffdb604b2a6e9c1c8d5e550318634ebf2e5e91
resent <contact@resent.dev>
2023-12-13 10:52:38 -0500

    [extendedmodlog] - update shit

--- extendedmodlog/__init__.py
+++ extendedmodlog/__init__.py
@@ -3,10 +3,6 @@ from pathlib import Path
 
 from .extendedmodlog import ExtendedModLog
 
-
-
-
 async def setup(bot):
     cog = ExtendedModLog(bot)
-    await cog.initialize()
     await bot.add_cog(cog)
