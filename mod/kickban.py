3552d8b1418b056654c0e8ac9ffd0d389fbf4903
resent <contact@resent.dev>
2023-12-16 01:33:59 -0500

    ok

--- mod/kickban.py
+++ mod/kickban.py
@@ -331,8 +331,8 @@ class KickBanMixin(MixinMeta):
                 )
             )
 
+    @commands.command(aliases="b")
     @commands.guild_only()
-    @commands.commands(aliases=["b"])
     @commands.has_permissions(ban_members=True)
     async def ban(
         self,
