68cec647a0493c9ec51ab73bcfd1e544824ea49c
resent <contact@resent.dev>
2023-12-15 23:00:52 -0500

    wtf

--- roletools/__init__.py
+++ roletools/__init__.py
@@ -1,11 +1,5 @@
-import json
-from pathlib import Path
 
 from .roletools import RoleTools
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 async def setup(bot):
     cog = RoleTools(bot)
