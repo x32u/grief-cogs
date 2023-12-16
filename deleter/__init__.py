a7dd40f6d6063e15073d89ad2f1eb03175b3c49b
resent <contact@resent.dev>
2023-12-16 00:01:50 -0500

    vscode being a bitch

--- deleter/__init__.py
+++ deleter/__init__.py
@@ -1,9 +1,5 @@
 from .deleter import Deleter
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
 
 async def setup(bot):
     cog = Deleter(bot)
