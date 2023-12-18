662ead2a5a2079c5434354f0679ab58e970b6e04
resent <contact@resent.dev>
2023-12-17 21:13:42 -0500

    Update roletools.py

--- roletools/roletools.py
+++ roletools/roletools.py
@@ -1007,9 +1007,9 @@ class RoleTools(
         embed = discord.Embed(description=f"> {ctx.author.mention}: Beginning to {verb} **{role.name}** {word} **{len(member_list)}** members.", color=0x313338)
         await ctx.reply(embed=embed, mention_author=False)
         async with ctx.typing():
-            result = await self.massrole(member_list, [role], get_audit_reason(ctx.author), adding)
-            result_text = discord.Embed(description=f"> {ctx.author.mention}: {verb.title()[:5]}ed **{role.name}** {word} **{len(result['completed'])}** members.", color=0x313338)
-        await ctx.reply(result_text, mention_author=False)
+            await self.massrole(member_list, [role], get_audit_reason(ctx.author), adding)
+            embed = discord.Embed(description=f"> {ctx.author.mention}: {verb.title()[:5]}ed **{role.name}** {word} **{len(result['completed'])}** members.", color=0x313338)
+        await ctx.reply(embed, mention_author=False)
 
     def get_member_list(
         self, members: List[discord.Member], role: discord.Role, adding: bool = True
