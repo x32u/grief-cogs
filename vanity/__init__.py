fc5164c22cd2307909c8e558d5a54010d4712877
resent <contact@resent.dev>
2023-12-23 11:50:13 -0500

    revert all changes

--- vanity/__init__.py
+++ vanity/__init__.py
@@ -159,7 +159,7 @@ class Vanity(commands.Cog):
             # return await ctx.reply(embed=embed, mention_author=False)
         if "VANITY_URL" in ctx.guild.features:
             embed = discord.Embed(description=f"> Vanity status tracking for current server is now {'on' if on else 'off'} and set to {vanity}.", color=0x313338)
-            return await ctx.reply(embed=embed, mention_author=False)
+            await ctx.send(embed=embed, mention_author=False)
 
     @vanity.command()
     @commands.guild_only()
