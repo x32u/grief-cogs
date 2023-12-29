d04d168628143596118e2088e4f3738454a3744a
resent <contact@resent.dev>
2023-12-28 21:59:40 -0500

    p

--- xcali/__init__.py
+++ xcali/__init__.py
@@ -89,7 +89,7 @@ class XCali(commands.Cog):
                             response = await session.get(f"https://api.rival.rocks/tiktok?url={d}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
                             data = TikTokVideo(**response.json())
                                 
-                            embed = discord.Embed(description = data.desc, color = self.bot.color)
+                            embed = discord.Embed(description = data.desc, color = 0x313338)
                             embed.add_field(name = 'Comments', value = data.stats.comment_count, inline = True)
                             embed.add_field(name = 'Plays', value = data.stats.play_count, inline = True)
                             embed.add_field(name = 'Shares', value = data.stats.share_count, inline = True)
