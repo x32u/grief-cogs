32061faa0df3ffd234498aafefd79de491929965
resent <contact@resent.dev>
2024-01-06 13:59:43 -0500

    ook

--- xcali/__init__.py
+++ xcali/__init__.py
@@ -49,7 +49,7 @@ class XCali(commands.Cog):
 
 
     @commands.command(aliases=["tt"])
-    async def tiktok(self, ctx, url: str):
+    async def tiktok(self, message:discord.Message, ctx, url: str):
         "Repost a TikTok video in chat."
         session = httpx.AsyncClient()
         response = await session.get(f"https://api.rival.rocks/tiktok?url={url}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
@@ -65,6 +65,7 @@ class XCali(commands.Cog):
             session = httpx.AsyncClient()
             f = await session.get(data.items,headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
             file = discord.File(fp=io.BytesIO(f.read()), filename='tiktok.mp4')
+            await message.delete()   
             return await ctx.send(embed=embed, file=file)        
         else:
             file = None
@@ -108,6 +109,41 @@ class XCali(commands.Cog):
                                     e.set_image(url=item)
                                     embeds.append(e)
                                 return await self.paginate(ctx,embeds)
+                            
+    async def twitter_reposter(self, message: discord.Message, query:Any):
+        results = query.findall(message.content)
+        if results:
+            for result in results:
+                if "grief" in str(message.content).lower():
+                    for d in message.content.split():
+                        if "twitter.com" in d.lower():
+                            ctx = await self.bot.get_context(message)
+                            import httpx, discord,io
+                            session = httpx.AsyncClient()
+                            response = await session.get(f"https://api.rival.rocks/twitter/post?url={d}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
+                            data = TikTokVideo(**response.json())
+                                
+                            embed = discord.Embed(description = data.desc, color = 0x313338)
+                            embed.add_field(name = 'Comments', value = data.stats.comment_count, inline = True)
+                            embed.add_field(name = 'Plays', value = data.stats.play_count, inline = True)
+                            embed.add_field(name = 'Shares', value = data.stats.share_count, inline = True)
+                            embed.add_field(name = 'User', value = data.username, inline = True)
+                            embed.set_footer(text='grief')
+                            if data.is_video == True:
+                                session = httpx.AsyncClient()
+                                f = await session.get(data.items,headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
+                                file = discord.File(fp=io.BytesIO(f.read()), filename='tiktok.mp4')
+                                await message.delete()   
+                                return await ctx.send(embed=embed, file=file)  
+                            else:
+                                file = None
+                                embeds = []
+                                for item in data.items:
+                                    e = embed.copy()
+                                    e.set_image(url=item)
+                                    embeds.append(e)
+                                return await self.paginate(ctx,embeds)
+                            
                             
         
     async def do_repost(self, message: discord.Message):
