91c31f6614ce408f3ff4badebca0593f6eb88544
resent <contact@resent.dev>
2024-01-06 13:37:28 -0500

    ook

--- xcali/__init__.py
+++ xcali/__init__.py
@@ -64,22 +64,6 @@ class TikTokPost(BaseModel):
     stats: Optional[VideoStats] = None
     url: Optional[str] = None
 
-class TwitterPostStatistics(BaseModel):
-    nsfw: Optional[str] = None
-    color: Optional[int] = 0
-    timestamp: Optional[int] = 0
-    text: Optional[str] = None
-    url: Optional[str] = None
-    like_count: Optional[int] = 0
-    retweet_count: Optional[int] = 0
-    raw_like_count: Optional[int] = 0
-    raw_retweet_count: Optional[int] = 0
-    footer_url: Optional[str] = 0
-    avatar: Optional[str] = None
-    screen_name: Optional[str] = None
-    image: Optional[str] = None
-    video: Optional[str] = None
-
 class XCali(commands.Cog):
     """
     Repost TikTok and YouTube videos.
@@ -117,63 +101,7 @@ class XCali(commands.Cog):
                 e.set_image(url=item)
                 embeds.append(e)
             return await self.paginate(ctx,embeds)
-        
-    @commands.command(aliases=["tw"])
-    async def twitter(self, ctx, url: str):
-        "Repost a TikTok video in chat."
-        session = httpx.AsyncClient()
-        response = await session.get(f"https://api.rival.rocks/twitter/post?url={url}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
-        data = TwitterPostStatistics(**response.json())
-            
-        embed = discord.Embed(description = data.text, color = 0x313338)
-        if data.video == True:
-            session = httpx.AsyncClient()
-            f = await session.get(data.items,headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
-            file = discord.File(fp=io.BytesIO(f.read()), filename='twitter.mp4')
-            return await ctx.send(embed=embed, file=file)        
-        else:
-
-            file = None
-            embeds = []
-            for item in data.items:
-                e = embed.copy()
-                e.set_image(url=item)
-                embeds.append(e)
-            return await self.paginate(ctx,embeds)
-        
-    async def reposter(self, message: discord.Message, query:Any):
-        results = query.findall(message.content)
-        if results:
-            for result in results:
-                if "tiktok" in str(message.content).lower():
-                    for d in message.content.split():
-                        if "tiktok.com" in d.lower():
-                            ctx = await self.bot.get_context(message)
-                            import httpx, discord,io
-                            session = httpx.AsyncClient()
-                            response = await session.get(f"https://api.rival.rocks/tiktok?url={d}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
-                            data = TikTokPost(**response.json())
-                                
-                            embed = discord.Embed(description = data.description, color = 0x313338)
-                            embed.add_field(name = 'Comments', value = data.stats.comment_count, inline = True)
-                            embed.add_field(name = 'Plays', value = data.stats.play_count, inline = True)
-                            embed.add_field(name = 'Shares', value = data.stats.share_count, inline = True)
-                            embed.add_field(name = 'User', value = data.author, inline = True)
-                            embed.set_footer(text='grief')
-                            if data.is_video == True:
-                                session = httpx.AsyncClient()
-                                f = await session.get(data.items,headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
-                                file = discord.File(fp=io.BytesIO(f.read()), filename='tiktok.mp4')
-                                await message.delete()   
-                                return await ctx.send(embed=embed, file=file)  
-                            else:
-                                file = None
-                                embeds = []
-                                for item in data.items:
-                                    e = embed.copy()
-                                    e.set_image(url=item)
-                                    embeds.append(e)
-                                return await self.paginate(ctx,embeds)
+    
                             
         
     async def do_repost(self, message: discord.Message):
