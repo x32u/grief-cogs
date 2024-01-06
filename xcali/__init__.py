41a57ee1cedee8255ea3db67d407e824414b51ed
resent <contact@resent.dev>
2024-01-06 13:42:53 -0500

    ook

--- xcali/__init__.py
+++ xcali/__init__.py
@@ -80,27 +80,27 @@ class XCali(commands.Cog):
         "Repost a TikTok video in chat."
         session = httpx.AsyncClient()
         response = await session.get(f"https://api.rival.rocks/tiktok?url={url}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
-        data = VideoStats(**response.json())
+        data = TikTokPost(**response.json())
             
-        embed = discord.Embed(description = data.description, color = 0x313338)
-        embed.add_field(name = 'Comments', value = data.stats.comment_count, inline = True)
-        embed.add_field(name = 'Plays', value = data.stats.play_count, inline = True)
-        embed.add_field(name = 'Shares', value = data.stats.share_count, inline = True)
-        embed.add_field(name = 'User', value = data.author, inline = True)
-        embed.set_footer(text='grief')
+        # embed = discord.Embed(description = data.description, color = 0x313338)
+        # embed.add_field(name = 'Comments', value = data.stats.comment_count, inline = True)
+        # embed.add_field(name = 'Plays', value = data.stats.play_count, inline = True)
+        # embed.add_field(name = 'Shares', value = data.stats.share_count, inline = True)
+        # embed.add_field(name = 'User', value = data.author, inline = True)
+        # embed.set_footer(text='grief')
         if data.is_video == True:
             session = httpx.AsyncClient()
             f = await session.get(data.url,headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
             file = discord.File(fp=io.BytesIO(f.read()), filename='tiktok.mp4')
-            return await ctx.send(embed=embed, file=file)        
+            return await ctx.send(file=file)        
         else:
             file = None
-            embeds = []
-            for item in data.items:
-                e = embed.copy()
-                e.set_image(url=item)
-                embeds.append(e)
-            return await self.paginate(ctx,embeds)
+            # embeds = []
+            # for item in data.items:
+                # e = embed.copy()
+                # e.set_image(url=item)
+                # embeds.append(e)
+            # return await self.paginate(ctx,embeds)
     
                             
         
