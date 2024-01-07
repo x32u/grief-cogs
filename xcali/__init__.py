7e0372d94aa9f0615c97d05ca2a80c9d33c2556f
resent <contact@resent.dev>
2024-01-06 21:17:54 -0500

    ok

--- xcali/__init__.py
+++ xcali/__init__.py
@@ -154,7 +154,7 @@ class XCali(commands.Cog):
         response = await session.get(f"https://api.rival.rocks/twitter/post?url={url}&api-key=05eab8f3-f0f6-443b-9d5e-fba1339c4b04", headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) 20100101 Firefox/103.0"})
         data = TwitterPostResponse(**response.json())
             
-        embed = discord.Embed(description = data.desc, color = 0x313338)
+        embed = discord.Embed(description = data.media, color = 0x313338)
         embed.add_field(name = 'Replies', value = data.stats.replies, inline = True)
         embed.add_field(name = 'Views', value = data.stats.views, inline = True)
         embed.add_field(name = 'Retweets', value = data.stats.retweets, inline = True)
