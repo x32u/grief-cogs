4c0984e908e39210ba9fd2ce850f3c142de97540
resent <contact@resent.dev>
2024-01-11 16:04:05 -0500

    ok

---
+++ cmdlog/__init__.py
@@ -0,0 +1,16 @@
+import contextlib
+import importlib
+import json
+from pathlib import Path
+
+from grief.core import VersionInfo
+from grief.core.bot import Red
+
+from . import vexutils
+from .cmdlog import CmdLog
+from .vexutils.meta import out_of_date_check
+
+async def setup(bot: Red):
+    cog = CmdLog(bot)
+    await out_of_date_check("cmdlog", cog.__version__)
+    await bot.add_cog(cog)
