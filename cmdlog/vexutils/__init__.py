2358664c1c6a3ee812b7ba005f08f30a3d63dc4e
resent <contact@resent.dev>
2024-01-11 16:06:52 -0500

    [cmdlog] - fix imports

--- cmdlog/vexutils/__init__.py
+++ cmdlog/vexutils/__init__.py
@@ -5,4 +5,3 @@ from grief.core.bot import Grief
 
 from .chat import humanize_bytes, inline_hum_list, no_colour_rich_markup
 from .meta import format_help, format_info, get_vex_logger, out_of_date_check
-from .version import __version__
