a8b4152af8f86f681a8c368a5b3d2b0201afbce0
resent <contact@resent.dev>
2024-01-13 15:27:40 -0500

    ok

--- fun/fun.py
+++ fun/fun.py
@@ -27,6 +27,8 @@ from .core import Core
 from uwuipy import uwuipy
 import textwrap
 import asyncio
+from grief.core.utils.menus import DEFAULT_CONTROLS, menu
+
 
 
 _ = T_ = Translator("General", __file__)
@@ -247,94 +249,66 @@ class Fun(commands.Cog):
         """Search the Urban Dictionary.
 
         This uses the unofficial Urban Dictionary API.
+
         """
-
         try:
-            url = "https://api.urbandictionary.com/v0/define"
-
             params = {"term": str(word).lower()}
+
+            url = "https://api.urbandictionary.com/v0/define"
 
             headers = {"content-type": "application/json"}
 
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, headers=headers, params=params) as response:
-                    data = await response.json()
+            async with self.bot.aio.get(url, headers=headers, params=params) as response:
+                data = await response.json()
 
         except aiohttp.ClientError:
-            await ctx.send(
-                _("No Urban Dictionary entries were found, or there was an error in the process.")
-            )
+            await ctx.send("No Urban Dictionary entries were found, or there was an error in the process.")
             return
 
         if data.get("error") != 404:
             if not data.get("list"):
-                return await ctx.send(_("No Urban Dictionary entries were found."))
+                return await ctx.send("No Urban Dictionary entries were found.")
             if await ctx.embed_requested():
                 # a list of embeds
                 embeds = []
                 for ud in data["list"]:
                     embed = discord.Embed(color=await ctx.embed_color())
-                    title = _("{word} by {author}").format(
-                        word=ud["word"].capitalize(), author=ud["author"]
-                    )
+                    title = f"{ud['word'].capitalize()} by {ud['author']}"
                     if len(title) > 256:
-                        title = "{}...".format(title[:253])
+                        title = f"{title[:253]}..."
                     embed.title = title
                     embed.url = ud["permalink"]
 
-                    description = _("{definition}\n\n**Example:** {example}").format(**ud)
+                    description = ("{definition}\n\n**Example:** {example}").format(**ud)
                     if len(description) > 2048:
-                        description = "{}...".format(description[:2045])
+                        description = f"{description[:2045]}..."
                     embed.description = description
 
-                    embed.set_footer(
-                        text=_(
-                            "{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary."
-                        ).format(**ud)
-                    )
+                    embed.set_footer(text=("{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary.").format(**ud))
                     embeds.append(embed)
 
-                if embeds is not None and len(embeds) > 0:
-                    await menu(
-                        ctx,
-                        pages=embeds,
-                        message=None,
-                        page=0,
-                        timeout=30,
-                    )
+                if embeds is not None and embeds:
+                    await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
             else:
                 messages = []
                 for ud in data["list"]:
                     ud.setdefault("example", "N/A")
-                    message = _(
-                        "<{permalink}>\n {word} by {author}\n\n{description}\n\n"
-                        "{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary."
-                    ).format(
-                        word=ud.pop("word").capitalize(),
-                        description="{description}",
-                        **ud,
-                    )
+                    message = (
+                        "<{permalink}>\n {word} by {author}\n\n{description}\n\n{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary."
+                    ).format(word=ud.pop("word").capitalize(), description="{description}", **ud)
                     max_desc_len = 2000 - len(message)
 
-                    description = _("{definition}\n\n**Example:** {example}").format(**ud)
+                    description = ("{definition}\n\n**Example:** {example}").format(**ud)
                     if len(description) > max_desc_len:
-                        description = "{}...".format(description[: max_desc_len - 3])
+                        description = f"{description[: max_desc_len - 3]}..."
 
                     message = message.format(description=description)
                     messages.append(message)
 
-                if messages is not None and len(messages) > 0:
-                    await menu(
-                        ctx,
-                        pages=messages,
-                        message=None,
-                        page=0,
-                        timeout=30,
-                    )
+                if messages is not None and messages:
+                    await menu(ctx, pages=messages, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
         else:
-            await ctx.send(
-                _("No Urban Dictionary entries were found, or there was an error in the process.")
-            )
+            await ctx.send("No Urban Dictionary entries were found, or there was an error in the process.")
 
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
