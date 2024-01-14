373b8341819e3eca936da7d1f276dbeeadc7112d
resent <contact@resent.dev>
2024-01-13 19:16:33 -0500

    ok

--- info/info.py
+++ info/info.py
@@ -1496,7 +1496,7 @@ class Info(commands.Cog):
     
     @commands.command(aliases=["sp"])
     @commands.cooldown(1, 3, commands.BucketType.user)
-    async def spotify(self, ctx, user: discord.Member = None):
+    async def lol(self, ctx, user: discord.Member = None):
         "Sends what you or another user is listening to on Spotify."
         try:
             if user == None:
