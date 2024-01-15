680ba6cdd16cff0b6b1d657e30c9f2cd94e035d1
resent <contact@resent.dev>
2024-01-15 11:46:12 +0000

    [mod] - fix shit with userinfo

--- mod/names.py
+++ mod/names.py
@@ -273,7 +273,7 @@ class ModInfo(MixinMeta):
         avatar = member.display_avatar.replace(static_format="png")
         data.set_author(name=f"{statusemoji} {name}", url=avatar)
         data.set_thumbnail(url=avatar)
-        user = await self.bot.fetch_user(member)
+        user = await self.bot.fetch_user(member.id)
         if user.banner is not None:
             data.set_image(url=user.banner.url)
         await ctx.reply(embed=data, view=view, mention_author=False)
