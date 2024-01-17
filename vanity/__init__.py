533c73add39a279f50cbd6237ff30c06f9ebd09b
resent <contact@resent.dev>
2024-01-17 20:54:36 +0000

    Update __init__.py

--- vanity/__init__.py
+++ vanity/__init__.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import typing
 from logging import Logger, getLogger
@@ -24,8 +23,7 @@ class Vanity(commands.Cog):
         self.config.register_global(**default_guild)
         self.settings = {}
         self.first_run = True
-        self.vanity_cache = {}
-        self.update_cache()
+        self.vanity_cache = update_cache(self)
 
     async def update_cache(self):
         await self.bot.wait_until_red_ready()
@@ -35,112 +33,8 @@ class Vanity(commands.Cog):
             if vanity:
                 return self.vanity_cache[x] = vanity
         
-        return self.vanity_cache
-
-    async def safe_send(self, channel: discord.TextChannel, embed: discord.Embed) -> None:
-        try:
-            await channel.send(embed=embed)
-        except (discord.Forbidden, discord.HTTPException) as e:
-            self.logger.warning(
-                f"Failed to send message to {channel.name} in {channel.guild.name}/{channel.guild.id}: {str(e)}"
-            )
-
-    @commands.Cog.listener(LISTENER_NAME)
-    async def on_vanity_trigger(self, before: discord.Member, after: discord.Member) -> None:
-        if not self.vanity_cache[after.guild.id]:
-            await self.update_cache()
-            
-        if before.bot:
-            return
-        guild: discord.Guild = after.guild
-        data = await self.config.guild(guild).all()
-        if not data["toggled"]:
-            return
-        if not data["role"] or not data["channel"]:
-            return
-        #if not "VANITY_URL" in guild.features:
-            #return
-        vanity: str = "/" + self.vanity_cache[guild.id]
-        role: discord.Role = guild.get_role(int(data["role"]))
-        log_channel: discord.TextChannel = guild.get_channel(int(data["channel"]))
-        if not role:
-            self.logger.info(f"Vanity role not found for {guild.name}/{guild.id}, skipping")
-            return
-        if not log_channel:
-            self.logger.info(f"Vanity log channel not found for {guild.name}/{guild.id}, skipping")
-            return
-        if role.position >= guild.me.top_role.position:
-            self.logger.info(f"Vanity role is higher than me in {guild.name}/{guild.id}, skipping")
-            return
-        
-        before_custom_activity: typing.List[discord.CustomActivity] = [
-            activity
-            for activity in before.activities
-            if isinstance(activity, discord.CustomActivity)
-        ]
-        after_custom_activity: typing.List[discord.CustomActivity] = [
-            activity
-            for activity in after.activities
-            if isinstance(activity, discord.CustomActivity)
-        ]
-        has_in_status_embed = discord.Embed(
-            color=0x2F3136,
-            description=f"Thanks {after.mention} for having {vanity} in your status.\nI rewarded you with {role.mention}",
-        )
-        has_in_status_embed.set_footer(
-            text=self.bot.user.name,
-            icon_url="https://cdn.discordapp.com/emojis/886356428116357120.gif",
-        )
-
-        if not before_custom_activity and after_custom_activity:
-            if after_custom_activity[0].name is not None:
-                if vanity.lower() in after_custom_activity[0].name.lower():
-                    if role.id not in after._roles:
-                        try:
-                            await after.add_roles(role)
-                        except (discord.Forbidden, discord.HTTPException) as e:
-... (122 lines left)
-Collapse
-init__.py
-10 KB
-Not nice to have no auto complete
-:sad:
-﻿
-import asyncio
-import typing
-from logging import Logger, getLogger
-
-import discord
-from grief.core import Config, commands
-from grief.core.bot import Grief
-
-LISTENER_NAME: str = "on_presence_update" if discord.version_info.major == 2 else "on_member_update"
-
-class Vanity(commands.Cog):
-    """For level 3 servers, award your users for advertising the vanity in their status. """
-
-    def format_help_for_context(self, ctx: commands.Context) -> str:
-        pre_processed = super().format_help_for_context(ctx)
-        return (f"{pre_processed}\n")
-
-    def __init__(self, bot: Grief):
-        self.bot: Grief = bot
-        self.logger: Logger = getLogger("grief.vanity")
-        self.config = Config.get_conf(self, identifier=12039492, force_registration=True)
-        default_guild = {"role": None, "toggled": False, "channel": None, "vanity": None,}
-        self.config.register_global(**default_guild)
-        self.settings = {}
-        self.first_run = True
-        self.vanity_cache = {}
-        self.update_cache()
-
-    async def update_cache(self):
-        await self.bot.wait_until_red_ready()
-        data = await self.config.all_guilds()
-        for x in data:
-            vanity = data[x]["vanity"]
-            if vanity:
-                return self.vanity_cache[x] = vanity
+        if not self.vanity_cache:
+            return {}
         
         return self.vanity_cache
 
@@ -328,5 +222,3 @@ async def setup(bot: Grief):
     cog = Vanity(bot)
     await discord.utils.maybe_coroutine(bot.add_cog, cog)
     asyncio.create_task(cog.update_cache())
-init__.py
-10 KB
