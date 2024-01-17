62ec17387e3f03a236555946b8776c9eedb30d80
resent <contact@resent.dev>
2024-01-17 21:36:36 +0000

    Update __init__.py

--- vanity/__init__.py
+++ vanity/__init__.py
@@ -174,11 +174,6 @@ class Vanity(commands.Cog):
     @commands.has_guild_permissions(manage_guild=True)
     async def role(self, ctx: commands.Context, role: discord.Role) -> None:
         """Setup the role to be rewarded."""
-        if role.position >= ctx.author.top_role.position:
-            await ctx.send(
-                "Your role is lower or equal to the vanity role, please choose a lower role than yourself."
-            )
-            return
         if role.position >= ctx.guild.me.top_role.position:
             await ctx.send("The role is higher than me, please choose a lower role than me.")
         if ctx.guild.owner:
