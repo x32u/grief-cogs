35462c62ff61322fb4cd6513dd1290467e223ae6
resent <contact@resent.dev>
2024-01-25 00:42:15 -0500

    ok

--- ok/__init__.py
+++ ok/__init__.py
@@ -16,7 +16,7 @@ class Ok(commands.Cog):
         pre_processed = super().format_help_for_context(ctx)
         return (f"{pre_processed}\n")
     
-    def __init__(self, bot):
+    def __init__(self, bot: Grief):
         self.bot: Grief = bot
         self.topggtoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxNjkzOTI5NzAwOTQzNDY1NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNzA2MTU5NTM4fQ.sv_iYnZzcTMoSRm3Q7TPRBY517VPxDYRo6HKms2ksXU"  # Set this to your bot's Top.gg token
         self.topgg_client = topgg.DBLClient(self.bot, self.topggtoken, autopost=True)
