1b971fa1002f9f7fbe8c65997223570abb7a3262
resent <contact@resent.dev>
2024-01-25 00:18:29 -0500

    ok

---
+++ topgg/__init__.py
@@ -0,0 +1,33 @@
+from discord.ext import commands, tasks
+import topgg
+from dotenv import load_dotenv, dotenv_values
+from discord.ext.commands import Context
+from grief import Grief
+
+load_dotenv()
+dotenv_values()
+
+class TopgShit(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.dbl_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxNjkzOTI5NzAwOTQzNDY1NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNzA2MTU5NTM4fQ.sv_iYnZzcTMoSRm3Q7TPRBY517VPxDYRo6HKms2ksXU"  # Set this to your bot's Top.gg token
+        self.topggpy = topgg.DBLClient(self.bot, self.dbl_token, autopost=True, post_shard_count=True)
+        self.update_stats.start()
+        
+
+    @tasks.loop(minutes=25)
+    async def update_stats(self):
+        okkkk = self.bot.get_channel(1199945545117085746)
+        """This function runs every 30 minutes to automatically update your server count."""
+        try:
+            await self.topggpy.post_guild_count()
+            msg = await okkkk.send(f"**Top.gg API:** Posted server count ({self.topggpy.guild_count})")
+        except Exception as e:
+             msg = await okkkk.send(f"**Top.gg API:** Failed to post server count\n{e.__class__.__name__}: {e}")
+
+    @update_stats.before_loop
+    async def before_update_stats(self):
+        await self.bot.wait_until_red_ready()
+
+async def setup(bot: Grief):
+    await bot.add_cog(TopgShit(bot))  
