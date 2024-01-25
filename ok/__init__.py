cabfc7de205161db155ce55fb38f0318e2520cf5
resent <contact@resent.dev>
2024-01-25 00:59:33 -0500

    ok

--- ok/__init__.py
+++ ok/__init__.py
@@ -8,8 +8,9 @@ from grief.core.bot import Grief
 from grief.core import Config, commands
 from logging import Logger, getLogger
 
+class Ok(commands.Cog):
     
-def __init__(self, bot: Grief):
+    def __init__(self, bot: Grief):
         self.bot: Grief = bot
         self.logger: Logger = getLogger("grief.topgg")
         self.topggtoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxNjkzOTI5NzAwOTQzNDY1NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNzA2MTU5NTM4fQ.sv_iYnZzcTMoSRm3Q7TPRBY517VPxDYRo6HKms2ksXU"  # Set this to your bot's Top.gg token
@@ -19,21 +20,22 @@ def __init__(self, bot: Grief):
         self.update_stats.start()
         
 
-@tasks.loop(minutes=25)
-async def update_stats(self):
+    @tasks.loop(minutes=25)
+    async def update_stats(self):
         okkkk = self.bot.get_channel(1199945545117085746)
         """This function runs every 30 minutes to automatically update your server count."""
         try:
             await self.topgg.post_guild_count()
-            msg = await okkkk.send(f"**Top.gg API:** Posted server count ({self.topgg.guild_count})")
+            msg = await okkkk.send(f"**Top.gg API:** Posted server count)")
             await msg.add_reaction("<:grief_check:1107472942830456892>")
         except Exception as e:
              msg = await okkkk.send(f"**Top.gg API:** Failed to post server count\n{e.__class__.__name__}: {e}")
              await msg.add_reaction("<:grief_x:1107472962333978655>")
 
-@update_stats.before_loop
-async def before_update_stats(self):
+    @update_stats.before_loop
+    async def before_update_stats(self):
         await self.bot.wait_until_red_ready()
 
 async def setup(bot: Grief):
-    await discord.utils.maybe_coroutine(bot.add_cog)
+    cog = Ok(bot)
+    await discord.utils.maybe_coroutine(bot.add_cog, cog)
