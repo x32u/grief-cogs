54d5302671807bda40d6b2fe80d92c94e254c070
resent <contact@resent.dev>
2024-01-26 22:08:30 -0500

    [mod] - add back username tracking

--- usernames/names.py
+++ usernames/names.py
@@ -25,6 +25,10 @@ class Names(MixinMeta):
     default_member_settings = {"past_nicks": [], "perms_cache": {},}
     default_user_settings = {"past_names": [], "past_display_names": []}
 
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        pre_processed = super().format_help_for_context(ctx)
+        return (f"{pre_processed}\n")
+
     def __init__(self, bot: Grief):
         super().__init__()
         self.bot = bot
