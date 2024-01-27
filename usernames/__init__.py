403d2a3cda2c99c3ca7a678b1840206689184f52
resent <contact@resent.dev>
2024-01-26 22:07:08 -0500

    [mod] - add back username tracking

--- usernames/__init__.py
+++ usernames/__init__.py
@@ -1,6 +1,6 @@
 from grief.core.bot import Grief
-from .names import Usernames
+from .names import Names
 
 
 async def setup(bot: Grief) -> None:
-    await bot.add_cog(Usernames(bot))
+    await bot.add_cog(Names(bot))
