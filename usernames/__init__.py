7dea50d4c750bd02284f0aa569fc0b0e5c656b1c
resent <contact@resent.dev>
2024-01-26 23:05:30 -0500

    [roletools] - update role icon embed

--- usernames/__init__.py
+++ usernames/__init__.py
@@ -57,6 +57,9 @@ class Names(commands.Cog):
     @commands.command()
     async def names(self, ctx: commands.Context, *, member: discord.Member):
         """Show previous usernames, global display names, and server nicknames of a member."""
+        if member == None:
+            member = ctx.author
+            return
         usernames, display_names, nicks = await self.get_names(member)
         parts = []
         for header, names in (
