046bede081a0228a07ae9da489542035299c4a0c
resent <contact@resent.dev>
2024-01-31 20:00:44 -0500

    ok

--- dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-from grief.core.bot import Grief
-
-from .dashboard import Dashboard
-
-
-async def setup(bot: Grief):
-    cog = Dashboard(bot)
-    await bot.add_cog(cog)
-    await cog.initialize()
