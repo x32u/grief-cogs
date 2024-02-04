6ad731ecd1e27fbdfab99d242e9d3d7b17a8dc94
resent <contact@resent.dev>
2024-02-04 02:30:55 -0500

    [baron] - update invite

--- baron/baron.py
+++ baron/baron.py
@@ -681,11 +681,11 @@ class Baron(commands.Cog):
         data = self.settings_cache
         
         if guild.id in data["whitelist"]:
-            await self.notify_guild(guild, f"Hello, I am grief. I am a free mutlipurpose Discord bot that features vanity rewards, TikTok/YouTube reposting, and pretty much everything you'd need. Below you can find my links:\n\n Community server — https://discord.gg/yor\n\n Support Server — https://discord.gg/KVbq2pDD5Jn\n Commands — <https://grief.cloud/commands>\n\n Docs — <https://docs.grief.cloud>",)
+            await self.notify_guild(guild, f"Hello, I am grief. I am a free mutlipurpose Discord bot that features vanity rewards, TikTok/YouTube reposting, and pretty much everything you'd need. Below you can find my links:\n\n Community server — https://discord.gg/yor\n\n Support Server — https://discord.gg/seer\n Commands — <https://grief.cloud/commands>\n\n Docs — <https://docs.grief.cloud>",)
             return
         
         elif guild.id in data["blacklist"]:
-            await self.notify_guild(guild, f"This server has been blacklisted, please join https://discord.gg/KVbq2pDD5J for support.",)
+            await self.notify_guild(guild, f"This server has been blacklisted, please join https://discord.gg/seer for support.",)
             await guild.leave()
             await self.baron_log("bl_leave", guild=guild)
 
@@ -697,7 +697,7 @@ class Baron(commands.Cog):
             await guild.chunk()
         
         if data["min_members"] and guild.member_count < data["min_members"]:
-            await self.notify_guild(guild, f"you must have at least 50 members, if your server is active you can request a whitelist at https://discord.gg/KVbq2pDD5J"),
+            await self.notify_guild(guild, f"you must have at least 50 members, if your server is active you can request a whitelist at https://discord.gg/seer"),
             await guild.leave()
         else:
             await self.notify_guild(guild, f"Hello, I am grief. I am a free mutlipurpose Discord bot that features vanity rewards, TikTok/YouTube reposting, and pretty much everything you'd need. Below you can find my links:\n\n Community server — https://discord.gg/yor\n\n Support Server — https://discord.gg/KVbq2pDD5J\n\n Commands — <https://grief.cloud/commands>\n\n Docs — <https://docs.grief.cloud>"),
