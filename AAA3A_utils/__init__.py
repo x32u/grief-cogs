a88298023d161de990f420fe8f8cd855a923bc6e
resent <contact@resent.dev>
2024-02-07 09:55:24 -0500

    ok

---
+++ AAA3A_utils/__init__.py
@@ -0,0 +1,47 @@
+from . import cog
+from .cog import Cog
+from .cogsutils import CogsUtils
+from .context import Context
+from .loop import Loop
+from .menus import Menu, Reactions
+from .sentry import SentryHelper
+from .settings import Settings
+from .shared_cog import SharedCog
+from .views import (
+    Buttons,
+    ChannelSelect,
+    ConfirmationAskView,
+    Dropdown,
+    MentionableSelect,
+    Modal,
+    RoleSelect,
+    Select,
+    UserSelect,
+)  # NOQA
+
+cog.SharedCog = SharedCog
+
+from .version import __version__
+
+__author__ = "AAA3A"
+__version__ = __version__
+__all__ = [
+    "CogsUtils",
+    "Loop",
+    "SharedCog",
+    "Cog",
+    "Menu",
+    "Context",
+    "Settings",
+    "SentryHelper",
+    "ConfirmationAskView",
+    "Buttons",
+    "Dropdown",
+    "Select",
+    "ChannelSelect",
+    "MentionableSelect",
+    "RoleSelect",
+    "UserSelect",
+    "Modal",
+    "Reactions",
+]
